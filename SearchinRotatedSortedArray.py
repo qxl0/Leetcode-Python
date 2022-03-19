e788083536752162b1bc1085aa035a0925b7bd53
Qiang <qxl0@yahoo.com>
2022-03-19 19:57:32 -0400

    finished search in rotated sorted array

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -12,23 +12,38 @@ You must write an algorithm with O(log n) runtime complexity.
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
-        while l < r:
-            m = l + (r - l) // 2
-            if nums[m] > nums[r]:
-                l = m + 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                r = m  # nums[m] < nums[r], so m might be the one we want
-        return min(nums[l], nums[r])
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
 
 
 if __name__ == "__main__":
     s = Solution()
     # nums = [2, 3, -2, 4]
 
-    nums = [4, 5, 6, 7, 0, 1, 2]
     # nums = [3, 4, 5, 1, 2]
     # nums = [11, 13, 15, 17]
     # nums = [6, 1, 2, 3, 4, 5]
-    target = 0
+    # nums = [1, 3, 5]
+    # target = 1
+    # nums = [1, 3, 5]
+    # target = 5
+    nums = [4, 5, 6, 7, 0, 1, 2]
+    target = 3
+    # nums = [4, 5, 6, 7, 0, 1, 2]
+    # target = 0
     res = s.search(nums, target)
     print(res)
