b110be94e70b19a3bef6c301f0cb379baa16ac9a
Qiang <qxl0@yahoo.com>
2022-03-19 14:09:34 -0400

    Add maximum product subarray

---
+++ MaximumProductSubarray.py
@@ -0,0 +1,24 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        product = nums[0]
+        product2 = nums[0]
+        global_p = nums[0]
+        for i in range(1, len(nums)):
+            temp = max(nums[i], product * nums[i], product2 * nums[i])
+            product2 = min(nums[i], product2 * nums[i], product * nums[i])
+            product = temp
+            global_p = max(global_p, product, product2)
+        return global_p
+
+
+if __name__ == "__main__":
+    s = Solution()
+    # nums = [2, 3, -2, 4]
+
+    nums = [-4, -3, -2]
+    res = s.maxProduct(nums)
+    print(res)
