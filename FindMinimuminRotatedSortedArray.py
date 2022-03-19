39005a8362d86c86b0c924dedf6a467b23671385
Qiang <qxl0@yahoo.com>
2022-03-19 16:18:22 -0400

    Optimized by changing l = m + 1.

--- FindMinimuminRotatedSortedArray.py
+++ FindMinimuminRotatedSortedArray.py
@@ -1,3 +1,4 @@
+import bisect
 import sys
 from typing import List
 
@@ -5,21 +6,26 @@ from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
-        m = l + (r - l) // 2
-        while l < r and m != l:
+        while l < r:
+            m = l + (r - l) // 2
             if nums[m] > nums[r]:
-                l = m
+                l = m + 1
             else:
                 r = m
-            m = l + (r - l) // 2
         return min(nums[l], nums[r])
+
+    def findMin2(self, nums):
+        self.__getitem__ = lambda i: nums[i] <= nums[-1]
+        return nums[bisect.bisect(self, False, 0, len(nums))]
 
 
 if __name__ == "__main__":
     s = Solution()
     # nums = [2, 3, -2, 4]
 
+    nums = [3, 1, 2]
     # nums = [3, 4, 5, 1, 2]
-    nums = [11, 13, 15, 17]
+    # nums = [11, 13, 15, 17]
+    # nums = [6, 1, 2, 3, 4, 5]
     res = s.findMin(nums)
     print(res)
