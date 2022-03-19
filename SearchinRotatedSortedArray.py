57bdd648aff29d48addb3f8a9035a98e876b4367
Qiang <qxl0@yahoo.com>
2022-03-19 16:45:00 -0400

    Added Search in sorted rotated array

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -2,9 +2,15 @@ import bisect
 import sys
 from typing import List
 
+"""
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
 
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
         while l < r:
             m = l + (r - l) // 2
@@ -14,18 +20,15 @@ class Solution:
                 r = m  # nums[m] < nums[r], so m might be the one we want
         return min(nums[l], nums[r])
 
-    def findMin2(self, nums):
-        self.__getitem__ = lambda i: nums[i] <= nums[-1]
-        return nums[bisect.bisect(self, False, 0, len(nums))]
-
 
 if __name__ == "__main__":
     s = Solution()
     # nums = [2, 3, -2, 4]
 
-    nums = [3, 1, 2]
+    nums = [4, 5, 6, 7, 0, 1, 2]
     # nums = [3, 4, 5, 1, 2]
     # nums = [11, 13, 15, 17]
     # nums = [6, 1, 2, 3, 4, 5]
-    res = s.findMin(nums)
+    target = 0
+    res = s.search(nums, target)
     print(res)
