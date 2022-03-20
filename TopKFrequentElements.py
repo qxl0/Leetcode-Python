76a41271b179332ba5cead1ab04243946ff415bd
Qiang <qxl0@yahoo.com>
2022-03-20 18:57:55 -0400

    Solved topKFrequent

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -1,20 +1,42 @@
 from typing import List, Optional
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+import collections
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+        counter = collections.Counter(nums)
+        # counter.most_common()
+        return [*counter.keys()][:k]
+
+    def topKFrequent2(self, nums, k):
+        hs = {}
+        frq = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in hs:
+                hs[nums[i]] = 1
+            else:
+                hs[nums[i]] += 1
+
+        for z, v in hs.items():
+            if v not in frq:
+                frq[v] = [z]
+            else:
+                frq[v].append(z)
+        arr = []
+        # most freq - len(nums), so loop from high ---> 0 if in frq, append it
+        for x in range(len(nums), 0, -1):
+            if x in frq:
+                for i in frq[x]:
+                    arr.append(i)
+        return [arr[x] for x in range(k)]
 
 
 if __name__ == "__main__":
     s = Solution()
-    # lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
-    nums = [1, 1, 1, 2, 2, 3]
-    res = s.topKFrequent(nums)
+    # nums = [1, 1, 1, 2, 2, 3]
+    # k = 2
+    nums = [3, 0, 1, 0]
+    k = 1
+    res = s.topKFrequent2(nums, k)
     print(res)
