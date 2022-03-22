6e27e881a6f8140a6ee7bf94d6b2a7c1ff376690
Qiang <qxl0@yahoo.com>
2022-03-22 19:59:43 -0400

    Climbing Stairs

--- leetcode_190.py
+++ leetcode_190.py
@@ -1,10 +1,24 @@
 class Solution:
-    def reverse_bits(self, x):
+    def reverse_bits(self, n):
         res = 0
         while n > 0:
             lastbit = n & 1
             n = n >> 1
             res = res * 2 + lastbit
+        return res
+
+    def reverseBits2(self, n):
+        reversed = 0
+        for i in range(32):
+            reversed = reversed << 1
+            reversed |= (n >> i) & 0x1
+        return reversed
+
+    def reverseBits(self, n):
+        res = 0
+        for _ in range(32):
+            res = (res << 1) + (n & 1)
+            n >>= 1
         return res
 
     def print_bits(self, x):
@@ -21,5 +35,10 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-    x = 64
-    sol.reverse_bits(x)
+    # x = 64
+    # x = 0b00000010100101000001111010011100
+    x = 0b11111111111111111111111111111101
+    res2 = sol.reverseBits2(x)
+    res = sol.reverseBits(x)
+    print(res == res2)
+    sol.print_bits(res)
