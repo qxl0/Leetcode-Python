1594d01d76cd5ddc70e3f855655341b7bdf9991f
Qiang <qxl0@yahoo.com>
2022-03-21 20:26:25 -0400

    Solution added for three  sum

--- 3Sum.py
+++ 3Sum.py
@@ -1,9 +1,33 @@
+from typing import List
+
+
 class Solution:
-  def threeSum(self, nums: List[int]) -> List[List[int]]:
-    pass
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            # for each i, solve a 2 sum problem for 0-nums[i]
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[j] + nums[k] == -nums[i]:
+                    res.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif nums[j] + nums[k] > -nums[i]:
+                    k -= 1
+                else:
+                    j += 1
+        return res
+
 
 if __name__ == "__main__":
     s = Solution()
-    nums = [-1,0,1,2,-1,-4]
+    nums = [-1, 0, 1, 2, -1, -4]
     res = s.threeSum(nums)
     print(res)
