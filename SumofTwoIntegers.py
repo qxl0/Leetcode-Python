ff3316cb9ae9baa56f3331046785265fda2d51d1
Qiang <qxl0@yahoo.com>
2022-03-21 20:58:31 -0400

    Added getSum

--- SumofTwoIntegers.py
+++ SumofTwoIntegers.py
@@ -6,12 +6,17 @@ Given two integers a and b, return the sum of the two integers without using the
 
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        pass
+        mask = 0xFFFFFFFF
+        while (b & mask) > 0:
+            carry = (a & b) << 1
+            a = a ^ b
+            b = carry
+            return a & mask if b > 0 else a
 
 
 if __name__ == "__main__":
     s = Solution()
-    a = 1
+    a = 3
     b = 2
     res = s.getSum(a, b)
     print(res)
