ab5b0c63640113da7d0c95a6241b3693baab3a64
Qiang <qxl0@yahoo.com>
2022-03-22 16:07:33 -0400

    Add counting bits

---
+++ CountingBits.py
@@ -0,0 +1,19 @@
+"""
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+ 
+"""
+
+
+from re import I
+
+
+class Solution:
+    def countBits(self, n: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 5
+    res3 = s.countBits(n)
+    print(res3)
