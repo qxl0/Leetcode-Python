8583b172f18f4e908c276ff6d5ccce24d68bb4ce
Qiang <qxl0@yahoo.com>
2022-03-23 14:36:58 -0400

    Solve Combinatio Sum in DP

--- CombinationSumIV.py
+++ CombinationSumIV.py
@@ -11,7 +11,13 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        pass
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for i in range(target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp[target]
 
 
 if __name__ == "__main__":
