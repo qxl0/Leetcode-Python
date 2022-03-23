7d47f00298353bc47b216c39ec8a3d64b4cc300a
Qiang <qxl0@yahoo.com>
2022-03-22 20:01:58 -0400

    Coin Change

---
+++ CoinChange.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], n: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    n = 11
+    coins = [1, 2, 5]
+    res = sol.coinChange(n)
+    print(res)
