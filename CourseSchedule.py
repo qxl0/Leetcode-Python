7e65cb895939ddf27ea4ff3ccb52cab5c9990d7e
Qiang <qxl0@yahoo.com>
2022-03-24 16:46:54 -0400

    Solved Course schedule using DFS

--- CourseSchedule.py
+++ CourseSchedule.py
@@ -16,18 +16,43 @@ Return true if you can finish all courses. Otherwise, return false.
 """
 
 
+import collections
 from typing import List
 
 
 class Solution:
     # exceed time limits
     def canFinish(self, numCourses, prerequisites):
-        pass
+        # construct adj
+        adj = collections.defaultdict(list)
+        visited = set()
+        res = set()
+        for u, v in prerequisites:
+            adj[u].append(v)
+
+        def dfs(i, adj, visited, res):
+            if i in visited:
+                return
+            visited.add(i)
+            if not adj[i]:
+                res.add(i)
+                return
+            for pre in adj[i]:
+                dfs(pre, adj, visited, res)
+            canDo = all([p in res for p in adj[i]])
+            if canDo:
+                res.add(i)
+
+        for i in range(n):
+            dfs(i, adj, visited, res)
+        return len(res) == numCourses
 
 
 if __name__ == "__main__":
     sol = Solution()
-    n = 2
-    nums = [[1, 0]]
+    # n = 2
+    # nums = [[1, 0], [0, 1]]
+    n = 5
+    nums = [[0, 1], [0, 2], [1, 3], [1, 4], [3, 4]]
     res = sol.canFinish(n, nums)
     print("result is: ", res)
