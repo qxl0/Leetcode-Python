3ae6c8efd83072a1d967d64f5a34ef4e92ba81ad
Qiang <qxl0@yahoo.com>
2022-03-24 11:29:09 -0400

    Finish Jump Games

--- JumpGames.py
+++ JumpGames.py
@@ -21,11 +21,19 @@ from typing import List
 class Solution:
     # exceed time limits
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        dp = [False] * len(nums)
+        dp[0] = True
+        for i in range(1, len(nums)):
+            for j in range(i - 1, -1, -1):
+                if dp[j] and nums[j] >= i - j:
+                    dp[i] = True
+                    break
+        return dp[-1]
 
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [2, 3, 1, 1, 4]
+    # nums = [2, 3, 1, 1, 4]
+    nums = [3, 2, 1, 0, 4]
     res = sol.canJump(nums)
     print("result is: ", res)
