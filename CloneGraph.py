140bc9f41be5e52e00710b30302791f6b0155e4c
Qiang <qxl0@yahoo.com>
2022-03-24 11:31:43 -0400

    Clone Graph

---
+++ CloneGraph.py
@@ -0,0 +1,53 @@
+"""
+133. Clone Graph
+Medium
+
+5414
+
+2417
+
+Add to List
+
+Share
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+"""
+
+
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: "Node") -> "Node":
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    adjList = [[2, 4], [1, 3], [2, 4], [1, 3]]
+    res = sol.cloneGraph(adjList)
+    print("result is: ", res)
