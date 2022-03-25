4a93fd4eaed19eeb7116d443affdd1e00029e3fc
Qiang <qxl0@yahoo.com>
2022-03-25 10:15:49 -0400

    Solved Longest Consecutive Sequence

--- LongestConsecutiveSequence.py
+++ LongestConsecutiveSequence.py
@@ -19,11 +19,24 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        pass
+        longest = 0
+        num_set = set(nums)
+        for num in nums:
+            cur_len, j = 1, 1
+            while num - j in num_set:
+                num_set.remove(num - j)
+                cur_len, j = cur_len + 1, j + 1
+            j = 1
+            while num + j in num_set:
+                num_set.remove(num + j)
+                cur_len, j = cur_len + 1, j + 1
+            longest = max(longest, cur_len)
+        return longest
 
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [100, 4, 200, 1, 3, 2]
+    nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+    # nums = [100, 4, 200, 1, 3, 2]
     res = sol.longestConsecutive(nums)
     print("result is: ", res)
