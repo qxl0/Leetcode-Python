b61efe86a4a122ca811355012ec156f9a0e2f997
Qiang <qxl0@yahoo.com>
2022-03-25 09:26:59 -0400

    Solved Number of Islands

--- NumberOfIslands.py
+++ NumberOfIslands.py
@@ -22,7 +22,26 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        pass
+        if not grid:
+            return 0
+
+        def dfs(grid, x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] != "1":
+                return
+            grid[x][y] = "#"
+            directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+            for dir_x, dir_y in directions:
+                new_x, new_y = x + dir_x, y + dir_y
+                dfs(grid, new_x, new_y)
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    count += 1
+                    dfs(grid, i, j)
+        return count
 
 
 if __name__ == "__main__":
@@ -30,8 +49,8 @@ if __name__ == "__main__":
     grid = [
         ["1", "1", "1", "1", "0"],
         ["1", "1", "0", "1", "0"],
-        ["1", "1", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"],
+        ["1", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "0"],
     ]
     res = sol.numIslands(grid)
     print("result is: ", res)
