c78c37c6b97c6c9cc03c28a7d36813e6371a440d
Qiang <qxl0@yahoo.com>
2022-03-26 19:26:09 -0400

    Solved meeting room 3 by using scan lines alogorithm

--- MeetingRoom3.py
+++ MeetingRoom3.py
@@ -17,7 +17,50 @@ class Solution:
     def meeting_room_i_i_i(
         self, intervals: List[List[int]], rooms: int, ask: List[List[int]]
     ) -> List[bool]:
-        pass
+        # (position, is_start, ask_index)
+        merged_intervals = []
+        for start, end in intervals:
+            merged_intervals.append((start, True, -1))
+            merged_intervals.append((end, False, -1))
+        for index, (start, end) in enumerate(ask):
+            merged_intervals.append((start, True, index))
+            merged_intervals.append((end, False, index))
+
+        merged_intervals.sort()
+        self.meetings = 0
+        self.ask_set = set()
+        self.result = [True] * len(ask)
+        self.rooms = rooms
+        for pos, is_start, ask_index in merged_intervals:
+            if ask_index == -1:
+                self.handle_meeting(is_start)
+            else:
+                self.handle_ask(is_start, ask_index)
+
+        return self.result
+
+    def handle_meeting(self, is_start):
+        if not is_start:
+            self.meetings -= 1
+            return
+        self.meetings += 1
+        if self.meetings >= self.rooms:
+            self.mark_full()
+
+    def handle_ask(self, is_start, ask_index):
+        if is_start:
+            if self.meetings >= self.rooms:
+                self.result[ask_index] = False
+            else:
+                self.ask_set.add(ask_index)
+        else:
+            if ask_index in self.ask_set:
+                self.ask_set.remove(ask_index)
+
+    def mark_full(self):
+        for index in self.ask_set:
+            self.result[index] = False
+        self.ask_set.clear()
 
 
 if __name__ == "__main__":
@@ -25,5 +68,5 @@ if __name__ == "__main__":
     intervals = [[1, 2], [4, 5], [8, 10]]
     rooms = 1
     ask = [[2, 3], [3, 4]]
-    res = sol.meeting_room_i_i_i(intervals)
+    res = sol.meeting_room_i_i_i(intervals, rooms, ask)
     print("result is: ", res)
