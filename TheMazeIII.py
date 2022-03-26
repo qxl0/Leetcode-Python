baccc7c44752348e2197df65e7b224f4e46ef33a
Qiang <qxl0@yahoo.com>
2022-03-26 11:17:08 -0400

    Maze 3: another solution

--- TheMazeIII.py
+++ TheMazeIII.py
@@ -13,11 +13,19 @@ the shortest method in alphabetical order. If the ball doesn't go into the hole,
 The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze are all walls. 
 The ball and the hole coordinates are represented by row and column indexes.
 """
+import collections
 import heapq
 import sys
 from typing import (
     List,
 )
+
+DIRECTION_HASH = {
+    "d": (1, 0),
+    "l": (0, -1),
+    "r": (0, 1),
+    "u": (-1, 0),
+}
 
 
 class Solution:
@@ -78,17 +86,76 @@ class Solution:
         return ret if ret != "" else "impossible"
 
 
+class MazeGridType:
+    SPACE = 0
+    WALL = 1
+
+
+class Solution2:
+    """
+    @param maze: the maze
+    @param ball: the ball position
+    @param hole: the hole position
+    @return: the lexicographically smallest way
+    """
+
+    def findShortestWay(self, maze, ball, hole):
+        # corner case check
+        if not ball or not hole:
+            return "impossible"
+        if not maze or not maze[0]:
+            return "impossible"
+
+        hole = (hole[0], hole[1])
+
+        # (distance, x, y, path)
+        queue = collections.deque([(ball[0], ball[1])])
+        distance = {(ball[0], ball[1]): (0, "")}
+
+        while queue:
+            x, y = queue.popleft()
+            dist, path = distance[(x, y)]
+
+            for direction in DIRECTION_HASH:
+                if path and path[-1] == direction:
+                    continue
+
+                new_x, new_y = self.kick_ball(x, y, direction, maze, hole)
+                new_dist = dist + abs(new_x - x) + abs(new_y - y)
+                new_path = path + direction
+                if (new_x, new_y) in distance and distance[(new_x, new_y)] <= (
+                    new_dist,
+                    new_path,
+                ):
+                    continue
+
+                queue.append((new_x, new_y))
+                distance[(new_x, new_y)] = (new_dist, new_path)
+
+        if hole in distance:
+            return distance[hole][1]
+
+        return "impossible"
+
+    def kick_ball(self, x, y, direction, maze, hole):
+        # kick ball through direction from x, y and return the stopped position
+        dx, dy = DIRECTION_HASH[direction]
+        while (x, y) != hole and not self.is_wall(x, y, maze):
+            x += dx
+            y += dy
+
+        if (x, y) == hole:
+            return x, y
+
+        return x - dx, y - dy
+
+    def is_wall(self, x, y, maze):
+        if not (0 <= x < len(maze) and 0 <= y < len(maze[0])):
+            return True
+        return maze[x][y] == MazeGridType.WALL
+
+
 if __name__ == "__main__":
-    sol = Solution()
-    # maze = [
-    #     [0, 0, 0, 0, 0],
-    #     [1, 1, 0, 0, 1],
-    #     [0, 0, 0, 0, 0],
-    #     [0, 1, 0, 0, 1],
-    #     [0, 1, 0, 0, 0],
-    # ]
-    # ball = [4, 3]
-    # hole = [0, 1]
     maze = [
         [0, 0, 0, 0, 0],
         [1, 1, 0, 0, 1],
@@ -96,8 +163,18 @@ if __name__ == "__main__":
         [0, 1, 0, 0, 1],
         [0, 1, 0, 0, 0],
     ]
-    ball = [0, 0]
-    hole = [3, 3]
+    ball = [4, 3]
+    hole = [0, 1]
+    # maze = [
+    #     [0, 0, 0, 0, 0],
+    #     [1, 1, 0, 0, 1],
+    #     [0, 0, 0, 0, 0],
+    #     [0, 1, 0, 0, 1],
+    #     [0, 1, 0, 0, 0],
+    # ]
+    # ball = [0, 0]
+    # hole = [3, 3]
     # output "lul"
-    res = sol.find_shortest_way(maze, ball, hole)
+    sol = Solution2()
+    res = sol.findShortestWay(maze, ball, hole)
     print("result is: ", res)
