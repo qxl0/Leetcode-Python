a28d0f2b4f37a8d87553b7ff22355b8588d1ba78
Qiang <qxl0@yahoo.com>
2022-03-27 11:05:35 -0400

    Simple solution with 3 pointers

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -19,7 +19,16 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if not head:
+            return head
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
 
 
 if __name__ == "__main__":
