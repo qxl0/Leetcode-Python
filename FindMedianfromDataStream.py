67341a870058aa16f9606ba633900bec9893316e
Qiang <qxl0@yahoo.com>
2022-03-27 14:55:59 -0400

    Added a shorter, element solution to Find median from data stream

--- FindMedianfromDataStream.py
+++ FindMedianfromDataStream.py
@@ -11,7 +11,7 @@ MedianFinder() initializes the MedianFinder object.
 void addNum(int num) adds the integer num from the data stream to the data structure.
 double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
 """
-import heapq
+from heapq import *
 from lib2to3.pgen2.token import MINEQUAL
 
 
@@ -21,17 +21,53 @@ class MedianFinder:
         self.maxq = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.maxq, -num)
-        heapq.heappush(self.minq, -heapq.heappop(self.maxq))
+        heappush(self.maxq, -num)
+        heappush(self.minq, -heappop(self.maxq))
 
         if len(self.maxq) < len(self.minq):
-            heapq.heappush(self.maxq, -heapq.heappop(self.minq))
+            heappush(self.maxq, -heappop(self.minq))
 
     def findMedian(self) -> float:
         if len(self.minq) == len(self.maxq):
             return (self.minq[0] + -self.maxq[0]) / 2
         else:
             return -self.maxq[0]
+
+
+class MedianFinder2:
+    def __init__(self):
+        self.heaps = None, [], []
+        self.i = 1
+
+    def addNum(self, num):
+        heappush(self.heaps[-self.i], -heappushpop(self.heaps[self.i], num * self.i))
+        self.i *= -1
+
+    def findMedian(self):
+        return (self.heaps[self.i][0] * self.i - self.heaps[-1][0]) / 2.0
+
+
+class MedianFinder3:
+    def __init__(self):
+        self.data = 1, [], []
+
+    def addNum(self, num):
+        sign, h1, h2 = self.data
+        heappush(h2, -heappushpop(h1, num * sign))
+        self.data = -sign, h2, h1
+
+    def findMedian(self):
+        sign, h1, h2 = d = self.data
+        return (h1[0] * sign - d[-sign][0]) / 2.0
+
+
+class MedianFinder4:
+    def __init__(s):
+        h = [[], 1, -1, i := []]
+        s.addNum = (
+            lambda n: heappush(h[-1], -heappushpop(h[0], n * h[1])) or h.reverse()
+        )
+        s.findMedian = lambda: (h[0][0] * h[1] - i[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
@@ -41,13 +77,15 @@ class MedianFinder:
 
 if __name__ == "__main__":
     s = MedianFinder()
+    # s = MedianFinder3()
 
-    s.addNum(1)
+    s.addNum(7)
     s.addNum(2)
     s.addNum(20)
-    s.addNum(12)
-    s.addNum(4)
+    s.addNum(5)
+    # s.addNum(4)
     res = s.findMedian()
-    s.addNum(3)
+    s.addNum(23)
+    s.addNum(26)
     res = s.findMedian()
     print(res)
