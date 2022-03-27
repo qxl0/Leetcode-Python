79bec8817670845e77fd73df4a159a0f72ff8673
Qiang <qxl0@yahoo.com>
2022-03-27 10:59:40 -0400

    Add Reverse Linked List

---
+++ ReverseLinkedList.py
@@ -0,0 +1,30 @@
+"""
+Meeting Room 4
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] 
+(si < ei) and the value of each meeting. You can only attend a meeting at the same time. Please calculate the most value you can get.
+"""
+
+
+import collections
+import heapq
+import random
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    head = ListNode(1)
+    head.next = ListNode(2)
+    res = sol.reverseList(head)
+    print("result is: ", res)
