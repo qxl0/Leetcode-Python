a45eb0101e3a6196337cf5172e93569f960c51eb
Qiang <qxl0@yahoo.com>
2022-03-27 11:26:10 -0400

    Solved linked list cycle using slow,fast pointer

--- LinkedListCycle.py
+++ LinkedListCycle.py
@@ -22,7 +22,14 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        pass
+        if not head:
+            return False
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                return True
+        return False
 
 
 if __name__ == "__main__":
@@ -32,5 +39,5 @@ if __name__ == "__main__":
     head.next.next = ListNode(0)
     head.next.next.next = ListNode(4)
     head.next.next.next.next = head.next
-    res = sol.reverseList(head)
+    res = sol.hasCycle(head)
     print("result is: ", res)
