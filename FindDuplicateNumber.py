3f7efc5ea1e7fa4ecfe9f3639310a69216106aeb
Qiang <qxl0@yahoo.com>
2022-03-29 15:48:03 -0400

    Solved Duplicate number in another way

--- FindDuplicateNumber.py
+++ FindDuplicateNumber.py
@@ -24,8 +24,21 @@ class Solution:
                 return slow
 
 
+class Solution2:
+    def findDuplicate(self, nums: List[int]) -> int:
+        left, right = 1, len(nums) - 1
+        while left < right:
+            mid = (right + left) // 2
+            temp1 = [i <= mid for i in nums]
+            temp = sum(temp1)
+            left, right = [left, mid] if temp > mid else [mid + 1, right]
+        return right
+
+
 if __name__ == "__main__":
-    s = Solution()
+    s = Solution2()
     nums = [1, 3, 4, 2, 2]
+    # nums = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9]
+    # nums = [1, 2, 7, 4, 5, 6, 3, 7, 8, 9]
     res = s.findDuplicate(nums)
     print(res)
