532f2d824e43cd57b957010a5f9a63652a1f9f85
Qiang <qxl0@yahoo.com>
2022-03-28 20:52:13 -0400

    Letter Case Permutation

---
+++ LetterCasePermutation.py
@@ -0,0 +1,30 @@
+import math
+from typing import List
+
+"""
+784. Letter Case Permutation
+Medium
+
+3167
+
+141
+
+Add to List
+
+Share
+Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+
+Return a list of all possible strings we could create. Return the output in any order.
+"""
+
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    str = "a1b2"
+    res = s.letterCasePermutation(str)
+    print(res)
