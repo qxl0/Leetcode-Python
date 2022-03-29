54a17d49867a04121e0bef790fc984f0be0a7e98
Qiang <qxl0@yahoo.com>
2022-03-28 20:17:20 -0400

    solved word search 2

--- WordSearch_2.py
+++ WordSearch_2.py
@@ -58,6 +58,75 @@ class Solution:
             board[i][j] = c
 
 
+DIRS = [(-1, 0), (1, 0), (0, 1), (0 - 1)]
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        self.word = None
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_word = True
+        node.word = word
+
+    def search(self, word):
+        node = self.root
+        for c in word:
+            node = node.children.get(c)
+        if node is None:
+            return None
+        return node
+
+
+class Solution2:
+    def wordsearch2(self, board, words):
+        if not board or len(board) == 0:
+            return []
+        trie = Trie()
+        for w in words:
+            trie.add(w)
+        result = set()
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                c = board[i][j]
+                self.search(
+                    board, i, j, trie.root.children.get(c), set([(i, j)]), result
+                )
+        return list(result)
+
+    def search(self, board, x, y, node, visited, result):
+        if not node:
+            return
+        if node.is_word:
+            result.add(node.word)
+        for dx, dy in DIRS:
+            x_, y_ = x + dx, y + dy
+            if not self.inbounds(board, x_, y_):
+                continue
+            if (x_, y_) in visited:
+                continue
+            visited.add((x_, y_))
+            self.search(
+                board, x_, y_, node.children.get(board[x_][y_]), visited, result
+            )
+            visited.remove((x_, y_))
+
+    def inbounds(self, board, x, y):
+        return 0 <= x < len(board) and 0 <= y < len(board[0])
+
+
 if __name__ == "__main__":
     sol = Solution()
     board = [
