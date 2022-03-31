37e495be9fe3f2f34b407bc72eab3146d5cb1a7f
Qiang <qxl0@yahoo.com>
2022-03-31 16:18:05 -0400

    recursive with memo solution

--- SplitArrayLargestSum.py
+++ SplitArrayLargestSum.py
@@ -20,7 +20,24 @@ Write an algorithm to minimize the largest sum among these m subarrays.
 
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        pass
+        dp = {}
+
+        def dfs(i, m):
+            if m == 1:
+                return sum(nums[i:])
+            if (i, m) in dp:
+                return dp[(i, m)]
+            res, curSum = float("inf"), 0
+            for j in range(i, len(nums) - m + 1):
+                curSum += nums[j]
+                maxSum = max(curSum, dfs(j + 1, m - 1))
+                res = min(res, maxSum)
+                if curSum > res:
+                    break
+            dp[(i, m)] = res
+            return res
+
+        return dfs(0, m)
 
 
 if __name__ == "__main__":
