3f0358b1eb1da7cdd754cd88946c8c1d64cceaaa
Qiang <qxl0@yahoo.com>
2022-03-31 19:18:45 -0400

    Add a solution to Group anagrams

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def group_anagrams(self, strs):
         d = {}
@@ -7,8 +10,17 @@ class Solution:
         return d.values()
 
 
+class Solution2:
+    def group_anagrams(self, strs):
+        d = defaultdict(list)
+        for w in strs:
+            key = tuple(sorted(w))
+            d[key] += [w]
+        return d.values()
+
+
 if __name__ == "__main__":
-    sol = Solution()
+    sol = Solution2()
     strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
     res = sol.group_anagrams(strs)
     print(res)
