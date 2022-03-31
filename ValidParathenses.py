d66a25e81ffdb446b7f063bc625550a9d5a34a09
Qiang <qxl0@yahoo.com>
2022-03-31 19:28:54 -0400

    Add a stupid solution to valid parentheses

--- ValidParathenses.py
+++ ValidParathenses.py
@@ -1,3 +1,23 @@
+"""
+20. Valid Parentheses
+Easy
+
+12329
+
+540
+
+Add to List
+
+Share
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+"""
+
+
 class Solution:
     def isValid(self, s):
         d = {"(": ")", "[": "]", "{": "}"}
@@ -10,8 +30,18 @@ class Solution:
         return len(stack) == 0
 
 
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        while len(s) > 0:
+            l = len(s)
+            s = s.replace("()", "").replace("{}", "").replace("[]", "")
+            if l == len(s):
+                return False
+        return True
+
+
 if __name__ == "__main__":
-    sol = Solution()
+    sol = Solution2()
     # s = "()[]{}"
     s = "(()())"
     res = sol.isValid(s)
