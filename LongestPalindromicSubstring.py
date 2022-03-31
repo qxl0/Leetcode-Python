7c550f13bd125a2c1dc3f15f018f3c9262f789de
Qiang <qxl0@yahoo.com>
2022-03-31 19:57:54 -0400

    Solved Longest Palindrome Substring

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -15,11 +15,27 @@ Given a string s, return the longest palindromic substring in s.
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        res = ""
+
+        def findPalindromeFrom(s, l, r):
+            while l > 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l + 1 : r]
+
+        for i in range(len(s)):
+            tmp = findPalindromeFrom(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            tmp = findPalindromeFrom(s, i, i + 1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
 
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "babad"
+    # s = "babad"
+    s = "cbbd"
     res = sol.longestPalindrome(s)
     print(res)
