b2d18400402f4524b379772ff3254b7dcb3bf67f
Qiang <qxl0@yahoo.com>
2022-03-31 19:39:41 -0400

    Longest Palindromic Substring

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,25 @@
+"""
+5. Longest Palindromic Substring
+Medium
+
+16820
+
+984
+
+Add to List
+
+Share
+Given a string s, return the longest palindromic substring in s.
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "babad"
+    res = sol.longestPalindrome(s)
+    print(res)
