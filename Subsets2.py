ec5dc4101c6da314860c897213db50231185d40b
Qiang <qxl0@yahoo.com>
2022-03-31 07:24:48 -0400

    Subsets II

---
+++ Subsets2.py
@@ -0,0 +1,30 @@
+import math
+from typing import List
+
+"""
+90. Subsets II
+Medium
+
+4722
+
+147
+
+Add to List
+
+Share
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+"""
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1, 2, 2]
+    res = s.subsets(nums)
+    print(res)
