df2342010e4ad12f41a86abff8d1246f78368d47
Qiang <qxl0@yahoo.com>
2022-04-02 08:43:17 -0400

    Solved Permutation II

--- PermutationsII.py
+++ PermutationsII.py
@@ -23,12 +23,25 @@ class Solution:
     """
 
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        pass
+        res = []
+
+        def dfs(level, avail, curr):
+            if level == len(nums):
+                res.append(curr)
+                return
+            for i in range(len(avail)):
+                if i > 0 and avail[i] == avail[i - 1]:
+                    continue
+                dfs(level + 1, avail[:i] + avail[i + 1 :], curr + [avail[i]])
+
+        nums.sort()
+        dfs(0, nums, [])
+        return res
 
 
 if __name__ == "__main__":
 
     sol = Solution()
-    nums = [1, 2, 3]
+    nums = [1, 1, 3]
     res = sol.permuteUnique(nums)
     print(res)
