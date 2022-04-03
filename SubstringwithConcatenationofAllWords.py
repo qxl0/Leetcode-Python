6f01a922f33cc2384e39c325b19deaf7a06ee833
Qiang <qxl0@yahoo.com>
2022-04-03 07:39:11 -0400

    Increase range end by 1

--- SubstringwithConcatenationofAllWords.py
+++ SubstringwithConcatenationofAllWords.py
@@ -36,7 +36,7 @@ class Solution:
             if w not in allWords:
                 allWords[w] = 0
             allWords[w] += 1
-        for i in range(len(s) - wordLen * N):
+        for i in range(len(s) - wordLen * N + 1):
             hasWords = {}
             num = 0
             while num < N:
@@ -57,11 +57,47 @@ class Solution:
         return res
 
 
+class Solution2:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        wordLen = len(words[0])
+        N = len(words)
+        res = []
+        # set up the wordMap
+        wordMap = {}
+        for w in words:
+            if w not in wordMap:
+                wordMap[w] = 0
+            wordMap[w] += 1
+        # loop through s
+        for i in range(len(s) - wordLen * N + 1):
+            num = 0
+            map = {}  # word -> numoftimes
+            while num < N:
+                w = s[i + num * wordLen : i + (num + 1) * wordLen]
+                if w not in wordMap:
+                    break
+                # w is in wordMap, check
+                if w not in map:
+                    map[w] = 0
+                map[w] += 1
+                if map[w] > wordMap[w]:
+                    break
+                num += 1
+            if num == N:
+                res.append(i)
+
+        return res
+
+
 if __name__ == "__main__":
     sol = Solution()
-    # s = "barfoothefoobarman"
-    # words = ["foo", "bar"]
-    s = "barfoofoobarman"
-    words = ["foo", "bar", "foo"]
+    s = "wordgoodgoodgoodbestword"
+    words = ["word", "good", "best", "good"]
     res = sol.findSubstring(s, words)
     print(res)
+    # s = "barfoothefoobarman"
+    # words = ["foo", "bar"]
+    # s = "barfoofoobarman"
+    # words = ["foo", "bar", "foo"]
+    # res = sol.findSubstring(s, words)
+    # print(res)
