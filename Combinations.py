10f08fb14f89ecf5674e245b0834fb4996c60868
Qiang <qxl0@yahoo.com>
2022-04-02 22:03:07 -0400

    Two solutions added

--- Combinations.py
+++ Combinations.py
@@ -22,15 +22,35 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
 
-        def helper(level, curr):
+        def helper(level, avail, curr):
             if level == k:
                 res.append(curr.copy())
                 return
-            for i in range(1, n):
-                helper(level + 1, curr + [i])
+            for i in range(len(avail)):
+                helper(level + 1, avail[:i] + avail[i + 1 :], curr + [avail[i]])
 
-        helper(0, [])
+        helper(0, [i for i in range(1, n + 1)], [])
         return res
+
+    def combine2(self, n, k):
+        sol = []
+
+        def backtrack(remain, comb, nex):
+            # solution found
+            if remain == 0:
+                sol.append(comb.copy())
+            else:
+                # iterate through all possible candidates
+                for i in range(nex, n + 1):
+                    # add candidate
+                    comb.append(i)
+                    # backtrack
+                    backtrack(remain - 1, comb, i + 1)
+                    # remove candidate
+                    comb.pop()
+
+        backtrack(k, [], 1)
+        return sol
 
 
 if __name__ == "__main__":
@@ -38,4 +58,6 @@ if __name__ == "__main__":
     n = 4
     k = 2
     res = sol.combine(n, k)
+    res2 = sol.combine2(n, k)
     print(res)
+    print(res2)
