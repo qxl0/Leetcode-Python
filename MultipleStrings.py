bfbc6c01ae980725950e26c7fb6693da36eaccf2
Qiang <qxl0@yahoo.com>
2022-04-03 07:48:05 -0400

    Multiply strings

---
+++ MultipleStrings.py
@@ -0,0 +1,31 @@
+"""
+43. Multiply Strings
+Medium
+
+4230
+
+1663
+
+Add to List
+
+Share
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    num1 = "2"
+    num2 = "3"
+    res = sol.multiply(num1, num2)
+    print(res)
