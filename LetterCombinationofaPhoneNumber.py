8740bc7da26cd02647f9f5030e85ce9beb446c5e
Qiang <qxl0@yahoo.com>
2022-04-03 11:25:54 -0400

    Solved Letter combination of phone number

--- LetterCombinationofaPhoneNumber.py
+++ LetterCombinationofaPhoneNumber.py
@@ -20,11 +20,34 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        pass
+        phoneMap = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz",
+        }
+        res = []
+        if not digits:
+            return res
+
+        def helper(index, cur):
+            if len(cur) == len(digits):
+                res.append(cur)
+                return
+            str = phoneMap[int(digits[index])]
+            for c in str:
+                helper(index + 1, cur + c)
+
+        helper(0, "")
+        return res
 
 
 if __name__ == "__main__":
     sol = Solution()
-    digits = "23"
+    digits = "238"
     res = sol.letterCombinations(digits)
     print(res)
