7802418872c88b68f330e6cdf714975720abf9b9
Qiang <qxl0@yahoo.com>
2022-04-04 11:20:08 -0400

    Solved contains duplicate

--- ContainDuplicate.py
+++ ContainDuplicate.py
@@ -19,7 +19,11 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        pass
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
 
 
 if __name__ == "__main__":
