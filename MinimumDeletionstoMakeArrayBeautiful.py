5541c33b53d433edb4eaeff5e1f00316f6296c6c
Qiang <qxl0@yahoo.com>
2022-04-04 11:14:02 -0400

    Solved 2216

--- MinimumDeletionstoMakeArrayBeautiful.py
+++ MinimumDeletionstoMakeArrayBeautiful.py
@@ -28,11 +28,36 @@ from typing import List
 
 class Solution:
     def minDeletion(self, nums: List[int]) -> int:
-        pass
+        res = []
+        for n in nums:
+            if len(res) % 2 == 0 or n != res[-1]:
+                res.append(n)
+        return len(nums) - (len(res) - len(res) % 2)
+
+
+class Solution2:
+    def minDeletion(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+        i = 0
+        while i < n:
+            j = i + 1
+            #
+            while j < n and nums[j] == nums[i]:
+                j += 1
+                res += 1
+            if j < n:
+                i = j + 1
+            else:
+                res += 1
+                break
+        return res
 
 
 if __name__ == "__main__":
-    sol = Solution3()
-    s = "banana"
-    res = sol.longestDupSubstring(s)
+    sol = Solution2()
+    # nums = [1, 1, 2, 2, 3, 3]
+    nums = [1, 1, 1, 2, 3, 3]
+    # nums = [1, 1, 2, 3, 5]
+    res = sol.minDeletion(nums)
     print(res)
