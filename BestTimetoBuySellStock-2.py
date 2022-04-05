3513ea2a6ef5514a476f1866e9285425478c1875
Qiang <qxl0@yahoo.com>
2022-04-05 07:57:01 -0400

    Best time to buy and sell stocks 2

---
+++ BestTimetoBuySellStock-2.py
@@ -0,0 +1,42 @@
+"""
+122. Best Time to Buy and Sell Stock II
+Medium
+
+7216
+
+2351
+
+Add to List
+
+Share
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+"""
+from collections import defaultdict
+import collections
+from math import factorial
+from operator import itemgetter
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        l = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[l]:
+                maxP = max(maxP, prices[i] - prices[l])
+            else:
+                l = i
+        return maxP
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    prices = [7, 1, 5, 3, 6, 4]
+    res = sol.maxProfit(prices)
+    print(res)
