4d72d8ff0668cdbb79704ce683347260929b7d8d
Qiang <qxl0@yahoo.com>
2022-04-05 10:51:58 -0400

    Solved buy sell stocks iii

--- BestTimetoBuySellStock-3.py
+++ BestTimetoBuySellStock-3.py
@@ -24,7 +24,35 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        dp = [0 for i in range(len(prices))]
+        for i in range(1, 2 + 1):
+            pos = -prices[0]
+            profit = 0
+            for t in range(1, len(prices)):
+                pos = max(pos, dp[t] - prices[t])
+                profit = max(profit, pos + prices[t])
+                dp[t] = profit
+        return dp[len(prices) - 1]
+        # return profit
+
+
+class Solution2:
+    def maxProfit(self, prices):
+        if not prices:
+            return 0
+        s1, s2, s3, s4 = (
+            -prices[0],
+            -float("inf"),
+            -float("inf"),
+            -float("inf"),
+        )
+
+        for p in prices:
+            s1 = max(s1, -p)
+            s2 = max(s2, s1 + p)
+            s3 = max(s3, s2 - p)
+            s4 = max(s4, s3 + p)
+        return s4
 
 
 if __name__ == "__main__":
