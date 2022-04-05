4c017c748268c1b499908256a1d2bb8b1ea07ca7
Qiang <qxl0@yahoo.com>
2022-04-05 11:47:57 -0400

    Generate Parentheses

---
+++ GenerateParentheses.py
@@ -0,0 +1,25 @@
+"""
+22. Generate Parentheses
+Medium
+
+12317
+
+480
+
+Add to List
+
+Share
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+"""
+
+
+class Solution:
+    def generateParenthesis(n):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    n = 3
+    res = sol.generateParenthesis(n)
+    print(res)
--- ValidParathenses.py
+++ ValidParathenses.py
@@ -40,9 +40,26 @@ class Solution2:
         return True
 
 
+class Solution3:
+    def isValid(self, s: str) -> bool:
+        opened = ["[", "{", "("]
+        closed = ["]", "}", ")"]
+        stack = []
+        for c in s:
+            if c in opened:
+                stack.append(c)
+            else:
+                if len(stack) != 0 and stack[-1] == opened[closed.index(c)]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+
 if __name__ == "__main__":
-    sol = Solution2()
+    sol = Solution3()
     # s = "()[]{}"
-    s = "(()())"
+    # s = "(()())"
+    s = "()"
     res = sol.isValid(s)
     print(res)
