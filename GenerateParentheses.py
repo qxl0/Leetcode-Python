0aa1c543552f596ad9a29ab6459e18676d1716da
Qiang <qxl0@yahoo.com>
2022-04-05 13:54:06 -0400

    Check if a Parentheses String can be valid

--- GenerateParentheses.py
+++ GenerateParentheses.py
@@ -14,12 +14,24 @@ Given n pairs of parentheses, write a function to generate all combinations of w
 
 
 class Solution:
-    def generateParenthesis(n):
-        pass
+    def generateParenthesis(self, n):
+        res = []
+
+        def helper(left, right, curr):
+            print(f"l={left}, r={right}")
+            if len(curr) == 2 * n:
+                res.append(curr)
+            if 0 < left:
+                helper(left - 1, right, curr + "(")
+            if right > left and right > 0:
+                helper(left, right - 1, curr + ")")
+
+        helper(n, n, "")
+        return res
 
 
 if __name__ == "__main__":
     sol = Solution()
-    n = 3
+    n = 2
     res = sol.generateParenthesis(n)
     print(res)
