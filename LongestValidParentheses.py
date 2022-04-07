b9fdbec84c625ab6241ac061f5788e0f7b579e10
Qiang <qxl0@yahoo.com>
2022-04-06 21:00:43 -0400

    Solved Leetcode 32

--- LongestValidParentheses.py
+++ LongestValidParentheses.py
@@ -18,12 +18,25 @@ from this import d
 
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        pass
+        stack = [0]
+        longest = 0
+        for c in s:
+            if c == "(":
+                stack.append(0)
+            else:
+                if len(stack) > 1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+        return longest
 
 
 if __name__ == "__main__":
     sol = Solution()
-    s = ")()())"
-    s = "(()"
+    # s = ")()())"
+    # s = "(()"
+    s = "())"
     res = sol.longestValidParentheses(s)
     print(res)
