67e23c8d0274abcf764238697b65c3a875f58168
Qiang <qxl0@yahoo.com>
2022-04-07 08:21:31 -0400

    Two binary search solution

--- FindFirstandLastPositionElementinSortedArray.py
+++ FindFirstandLastPositionElementinSortedArray.py
@@ -23,12 +23,46 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        pass
+        rets = []
+        l, r = 0, len(nums) - 1
+        # if l == r == 0:
+        #     return [-1, -1]
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                r = m
+        if l == r and nums[l] == target:
+            rets.append(l)
+        else:
+            rets.append(-1)
+
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l + 1) // 2  # 0,1 => 0
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m
+        if l == r and nums[l] == target:
+            rets.append(l)
+        else:
+            rets.append(-1)
+
+        return rets
 
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [5, 7, 7, 8, 8, 10]
-    res = sol.searchRange(nums)
+    # nums = [5, 7, 7, 8, 8, 10]
+    # target = 8
+    nums = []
+    target = 0
+    res = sol.searchRange(nums, target)
 
     print(res)
