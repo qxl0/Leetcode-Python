27046e06980c91abd61845e88f4f602feb30070f
Qiang <qxl0@yahoo.com>
2022-04-08 22:22:50 -0400

    Try to solve search 2D Matrix

--- Searcha2DMatrix.py
+++ Searcha2DMatrix.py
@@ -22,12 +22,31 @@ from typing import List, Optional
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        pass
+        m, n = len(matrix), len(matrix[0])
+        l, r = 1, m * n - 1
+
+        def getValue(v):
+            return matrix[v // n][v % n]
+
+        while l < r:
+            mid = l + (r - l) // 2
+            val = getValue(mid)
+            if val == target:
+                return True
+            if val < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return True if getValue(l) == target else False
 
 
 if __name__ == "__main__":
     sol = Solution()
-    matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
-    target = 3
+    # matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+    # target = 3
+    # matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+    # target = 13
+    matrix = [[1]]
+    target = 0
     res = sol.searchMatrix(matrix, target)
     print(res)
