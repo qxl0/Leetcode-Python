9da24c2d3e62df33b2693f44ae4f9fbf8b705450
Qiang <qxl0@yahoo.com>
2022-04-09 15:17:47 -0400

    Leecode 2168

---
+++ UniqueSubstringsWithEqualDigitFrequency.py
@@ -0,0 +1,18 @@
+"""
+LeetCode 2168. Unique Substrings with Equal Digit Frequency
+return the number of unique substrings of s where every digit appears the same number of times
+"""
+
+from typing import List, Optional
+
+
+class Solution:
+    def equalDigitFrequency(s):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "1212"
+    res = sol.equalDigitFrequency(s)
+    print(res)
