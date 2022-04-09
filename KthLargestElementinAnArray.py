4e6864d366ff25abe79692c9a4073c468dd9aede
Qiang <qxl0@yahoo.com>
2022-04-09 19:31:47 -0400

    Solved

--- KthLargestElementinAnArray.py
+++ KthLargestElementinAnArray.py
@@ -15,18 +15,27 @@ Note that it is the kth largest element in the sorted order, not the kth distinc
 """
 
 from filecmp import cmp
+import heapq
 import math
 from typing import List, Optional
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pass
+        minq = []
+        for n in nums:
+            heapq.heappush(minq, -n)
+        for _ in range(k - 1):
+            heapq.heappop(minq)
+
+        return -minq[0]
 
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [3, 2, 1, 5, 6, 4]
-    k = 2
+    # nums = [3, 2, 1, 5, 6, 4]
+    # k = 2
+    nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+    k = 4
     res = sol.findKthLargest(nums, k)
     print(res)
