dfad1710bbdb68fb03c76cdc739cc8f5ff0d10ac
Qiang <qxl0@yahoo.com>
2022-04-09 19:17:42 -0400

    Solved

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -15,6 +15,7 @@ Assume the environment does not allow you to store 64-bit integers (signed or un
 """
 
 from filecmp import cmp
+import math
 from typing import List, Optional
 
 
@@ -25,8 +26,27 @@ class Solution:
         return s * r * (r < 2**31)
 
 
+class Solution2:
+    def reverse(self, x):
+        reverse = 0
+        max_int = pow(2, 31) - 1
+        min_int = pow(-2, 31)
+
+        while x != 0:
+            pop = x % 10 if x >= 0 else (abs(x) % 10) * -1
+            x = x // 10 if x >= 0 else math.ceil(x / 10)
+            if (reverse > max_int // 10) or (reverse == max_int // 10 and pop > 7):
+                return 0
+            if (reverse < math.ceil(min_int / 10)) or (
+                reverse == math.ceil(min_int / 10) and pop < -8
+            ):
+                return 0
+            reverse = reverse * 10 + pop
+        return reverse
+
+
 if __name__ == "__main__":
-    sol = Solution()
+    sol = Solution2()
     x = 123
     res = sol.reverse(x)
     print(res)
