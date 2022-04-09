b884aace0600ff9805891b201e580b7e8abc3e50
Qiang <qxl0@yahoo.com>
2022-04-08 22:45:07 -0400

    Time Based KeyValue Store

---
+++ TimeBasedKeyValueStore.py
@@ -0,0 +1,46 @@
+"""
+981. Time Based Key-Value Store
+Medium
+
+2001
+
+204
+
+Add to List
+
+Share
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+"""
+
+from statistics import quantiles
+from this import d
+from typing import List, Optional
+
+
+class TimeMap:
+    def __init__(self):
+        pass
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        pass
+
+    def get(self, key: str, timestamp: int) -> str:
+        pass
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+
+
+if __name__ == "__main__":
+    sol = TimeMap()
+    res = sol.set(matrix, target)
+    print(res)
