1f025e68ad17c713968805341c121d96b2e0564e
Qiang <qxl0@yahoo.com>
2022-04-10 10:12:24 -0400

    Wild Card Matching

---
+++ WildCardMatching.py
@@ -0,0 +1,33 @@
+"""
+543. Diameter of Binary Tree
+Easy
+
+7551
+
+475
+
+Add to List
+
+Share
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+"""
+
+
+from typing import Optional
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "adceb"
+    p = "*a*b"
+    res = s.isMatch(s, p)
+    print(res)
