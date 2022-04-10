a2932f8451bb64f31e71b83f3c67f571da51f49e
Qiang <qxl0@yahoo.com>
2022-04-10 14:28:47 -0400

    Solved wild card match

--- WildCardMatching.py
+++ WildCardMatching.py
@@ -22,12 +22,43 @@ from typing import Optional
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        dp = [[False for _ in range(len(p) + 1)] for _ in range(len(s) + 1)]
+        dp[0][0] = True
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == "*":
+                dp[0][j] = True
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] in {s[i - 1], "?"}:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == "*":
+                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+        return dp[-1][-1]
+
+
+class Solution2:
+    def isMatch(self, s, p):
+        dp = [[False for _ in range(len(p) + 1)] for i in range(len(s) + 1)]
+        dp[0][0] = True
+        for j in range(1, len(p) + 1):
+            if p[j - 1] != "*":
+                break
+            dp[0][j] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] in {s[i - 1], "?"}:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == "*":
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+        return dp[-1][-1]
 
 
 if __name__ == "__main__":
-    sol = Solution()
-    s = "adceb"
-    p = "*a*b"
-    res = s.isMatch(s, p)
+    sol = Solution2()
+    # s = "adceb"
+    # p = "*a*b"
+    s = "aab"
+    p = "c*a*b"
+    res = sol.isMatch(s, p)
     print(res)
