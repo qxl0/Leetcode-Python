e32670e73f2f77b8db642400487c71a1347a7af7
Qiang <qxl0@yahoo.com>
2022-04-10 20:06:11 -0400

    Prim's alogrithm

--- MinCosttoConnectAllPoints.py
+++ MinCosttoConnectAllPoints.py
@@ -17,12 +17,37 @@ Return the minimum cost to make all points connected. All points are connected i
 """
 
 
+import heapq
 from typing import List, Optional
 
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        pass
+        n = len(points)
+
+        adj = {i: [] for i in range(n)}
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        # Prim's
+        res = 0
+        visit = set()
+        minH = [[0, 0]]  # cost,pint
+        while minH:
+            cost, i = heapq.heappop(minH)
+            if i in visit:
+                continue
+            res += cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
+        return res
 
 
 if __name__ == "__main__":
