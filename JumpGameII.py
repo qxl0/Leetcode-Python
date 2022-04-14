0c35f060754df5899b713eb4f159f704ac20df32
Qiang <qxl0@yahoo.com>
2022-04-14 14:26:39 -0400

    Solved it using BFS one dimentio

--- JumpGameII.py
+++ JumpGameII.py
@@ -23,11 +23,22 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        pass
+        n, start, end, step = len(nums), 0, 0, 0
+
+        while end < n - 1:
+            step += 1
+            maxend = end + 1
+            for i in range(start, end + 1):
+                if i + nums[i] >= n - 1:
+                    return step
+                maxend = max(maxend, i + nums[i])
+            start, end = end + 1, maxend
+        return step
 
 
 if __name__ == "__main__":
     s = Solution()
-    nums = [3, 2, 1, 0, 4]
+    # nums = [3, 2, 1, 0, 4]
+    nums = [2, 3, 1, 1, 4]
     res = s.jump(nums)
     print("Ans is : ", res)
