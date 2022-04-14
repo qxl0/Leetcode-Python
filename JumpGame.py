97b0f79cd9403f018857c2c1b8037dda21344d85
Qiang <qxl0@yahoo.com>
2022-04-14 09:08:47 -0400

    Jump Game

---
+++ JumpGame.py
@@ -0,0 +1,29 @@
+"""
+55. Jump Game
+Medium
+
+10598
+
+613
+
+Add to List
+
+Share
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+"""
+import sys
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [2, 3, 1, 1, 4]
+    res = s.canJump(nums)
+    print("Ans is : ", res)
