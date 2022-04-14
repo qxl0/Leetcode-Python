1168e34e09144c3ed381b215acfb17199c2c62b0
Qiang <qxl0@yahoo.com>
2022-04-14 15:14:34 -0400

    Greedy algorithm solved gas station

--- GasStation.py
+++ GasStation.py
@@ -21,12 +21,25 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        pass
+        if sum(gas) < sum(cost):
+            return -1
+
+        total = 0
+        start = 0
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+
+            if total < 0:
+                start = i + 1
+                total = 0
+        return start
 
 
 if __name__ == "__main__":
     s = Solution()
-    gas = [1, 2, 3, 4, 5]
-    cost = [3, 4, 5, 1, 2]
+    # gas = [1, 2, 3, 4, 5]
+    # cost = [3, 4, 5, 1, 2]
+    gas = [2, 1, 1, 2]
+    cost = [1, 2, 2, 1]
     res = s.canCompleteCircuit(gas, cost)
     print("Ans is : ", res)
