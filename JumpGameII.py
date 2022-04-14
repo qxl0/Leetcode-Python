ebaf586b4c05e757e272d797c7b6c9138e729256
Qiang <qxl0@yahoo.com>
2022-04-14 11:46:16 -0400

    Jump Game II

---
+++ JumpGameII.py
@@ -0,0 +1,33 @@
+"""
+45. Jump Game II
+Medium
+
+7754
+
+289
+
+Add to List
+
+Share
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+You can assume that you can always reach the last index.
+"""
+import sys
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [3, 2, 1, 0, 4]
+    res = s.jump(nums)
+    print("Ans is : ", res)
