66b3af3a0d8679afed78f4a3bd8a5324821b8fc3
Qiang <qxl0@yahoo.com>
2022-04-14 08:01:05 -0400

    Solved 1292

--- MaximumSideLengthofSquarewithSumlessthanThreshold.py
+++ MaximumSideLengthofSquarewithSumlessthanThreshold.py
@@ -18,12 +18,50 @@ from typing import List
 
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        pass
+        m = len(mat)
+        n = len(mat[0])
+        presum = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        # populate presum
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                presum[i][j] = (
+                    presum[i][j - 1]
+                    + presum[i - 1][j]
+                    + mat[i - 1][j - 1]
+                    - presum[i - 1][j - 1]
+                )
+
+        def findOK(mid):
+            # calculate the sum
+            for i in range(mid, m + 1):
+                for j in range(mid, n + 1):
+                    sum = (
+                        presum[i][j]
+                        - presum[i][j - mid]
+                        - presum[i - mid][j]
+                        + presum[i - mid][j - mid]
+                    )
+                    if sum <= threshold:
+                        return True
+            return False
+
+        l, r = 0, min(m, n)
+        while l < r:
+            mid = r - (r - l) // 2
+            print(f"{l} - {r}")
+            if findOK(mid):
+                l = mid
+            else:
+                r = mid - 1
+        return l
 
 
 if __name__ == "__main__":
     s = Solution()
-    mat = [[1, 1, 3, 2, 4, 3, 2], [1, 1, 3, 2, 4, 3, 2], [1, 1, 3, 2, 4, 3, 2]]
-    threshold = 4
+    # mat = [[1, 1, 3, 2, 4, 3, 2], [1, 1, 3, 2, 4, 3, 2], [1, 1, 3, 2, 4, 3, 2]]
+    # threshold = 4
+    mat = [[18, 70], [61, 1], [25, 85], [14, 40], [11, 96], [97, 96], [63, 45]]
+    threshold = 40184
     res = s.maxSideLength(mat, threshold)
     print(res)
