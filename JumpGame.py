c131ac3f52cc69c48c900196667d0a1a3c131baa
Qiang <qxl0@yahoo.com>
2022-04-14 10:03:52 -0400

    Added Greedy solution to canJump

--- JumpGame.py
+++ JumpGame.py
@@ -31,9 +31,27 @@ class Solution:
         print(f"{dp}")
         return dp[-1]
 
+    def canJump2(self, nums):
+        m = 0
+        for i, n in enumerate(nums):
+            if i > m:
+                return False
+            m = max(m, i + n)
+        return True
+
+    def canJump3(self, nums):
+        goal = len(nums) - 1
+        for i in range(len(nums))[::-1]:
+            if i + nums[i] >= goal:
+                goal = i
+        return not goal
+
 
 if __name__ == "__main__":
     s = Solution()
+    nums = [3, 2, 1, 0, 4]
+    res = s.canJump3(nums)
+    print("Ans is : ", res)
     nums = [2, 3, 1, 1, 4]
-    res = s.canJump(nums)
+    res = s.canJump3(nums)
     print("Ans is : ", res)
