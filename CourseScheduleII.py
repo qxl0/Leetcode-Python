2e028698f23810abac78147276f80fa4edf8f00e
Qiang <qxl0@yahoo.com>
2022-04-15 17:09:43 -0400

    Solved course schedule II

--- CourseScheduleII.py
+++ CourseScheduleII.py
@@ -23,12 +23,31 @@ from typing import List
 class Solution:
     # exceed time limits
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        pass
+        indegree = {i: 0 for i in range(numCourses)}
+        adj = {i: [] for i in range(numCourses)}
+
+        for a, b in prerequisites:
+            indegree[a] += 1
+            adj[b].append(a)
+
+        q = collections.deque([c for c in indegree if not indegree[c]])
+        result = []
+        while q:
+            c = q.popleft()
+            if c in result:
+                continue
+            result.append(c)
+
+            for neigh in adj[c]:
+                indegree[neigh] -= 1
+                if not indegree[neigh]:
+                    q.append(neigh)
+        return result if len(result) == numCourses else []
 
 
 if __name__ == "__main__":
     sol = Solution()
-    numCourses = 2
-    prerequisites = [[1, 0]]
+    numCourses = 4
+    prerequisites = [[1, 0], [2, 0], [3, 1], [3, 2]]
     res = sol.findOrder(numCourses, prerequisites)
     print("result is: ", res)
