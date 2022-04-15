249b4c4952a2bd1b239b6e30caf5f17b743dc267
Qiang <qxl0@yahoo.com>
2022-04-15 09:18:35 -0400

    another solution: same two pass

--- PartitionLabels.py
+++ PartitionLabels.py
@@ -16,6 +16,7 @@ Note that the partition is done so that after concatenating all the parts in ord
 Return a list of integers representing the size of these parts.
 """
 import collections
+import enum
 import heapq
 import sys
 from typing import List
@@ -23,11 +24,36 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        pass
+        result, lastSeen, maxLastSeen, count = [], {}, 0, 0
+        for i, char in enumerate(s):
+            lastSeen[char] = i
+        for i, char in enumerate(s):
+            maxLastSeen = max(maxLastSeen, lastSeen[char])
+            count += 1
+            if i == maxLastSeen:
+                result.append(count)
+                count = 0
+        return result
+
+
+class Solution2:
+    def partitionLabels(self, s):
+        last = {c: i for i, c in enumerate(s)}
+
+        start = end = 0
+        output = []
+
+        for i, c in enumerate(s):
+            end = max(end, last[c])
+
+            if i == end:
+                output.append(end - start + 1)
+                start = i + 1
+        return output
 
 
 if __name__ == "__main__":
-    s = Solution()
+    s = Solution2()
     str = "ababcbacadefegdehijhklij"
     res = s.partitionLabels(str)
     print("Ans is : ", res)
