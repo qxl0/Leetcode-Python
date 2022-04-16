94fda050b5e31e220274439822420c249512162c
Qiang <qxl0@yahoo.com>
2022-04-16 07:31:50 -0400

    Solved hammingWeight

--- Numberof1Bits.py
+++ Numberof1Bits.py
@@ -26,7 +26,11 @@ from typing import List
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        pass
+        count = 0
+        while n:
+            n = n & (n - 1)
+            count += 1
+        return count
 
 
 if __name__ == "__main__":
