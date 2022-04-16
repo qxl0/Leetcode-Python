5a10152c6c7d692c899c945d74295dcc8bb5a580
Qiang <qxl0@yahoo.com>
2022-04-15 21:43:52 -0400

    Solved

--- WordLadder.py
+++ WordLadder.py
@@ -26,13 +26,39 @@ from typing import List
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        pass
+        def construct_dict(word_list):
+            d = {}
+            for word in word_list:
+                for i in range(len(word)):
+                    s = word[:i] + "_" + word[i + 1 :]
+                    d[s] = d.get(s, []) + [word]
+            return d
+
+        def bfs_words(begin, end, dict_words):
+            queue, visited = collections.deque([(begin, 1)]), set()
+            while queue:
+                word, steps = queue.popleft()
+                if word not in visited:
+                    visited.add(word)
+                    if word == end:
+                        return steps
+                    for i in range(len(word)):
+                        s = word[:i] + "_" + word[i + 1 :]
+                        neigh_words = dict_words.get(s, [])
+                        for neigh in neigh_words:
+                            if neigh not in visited:
+                                queue.append((neigh, steps + 1))
+            return 0
+
+        d = construct_dict(set(wordList) | set([beginWord, endWord]))
+        return bfs_words(beginWord, endWord, d)
 
 
 if __name__ == "__main__":
     sol = Solution()
     beginWord = "hit"
     endWord = "cog"
-    wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+    # wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+    wordList = ["hot", "dot", "dog", "lot", "log"]
     res = sol.ladderLength(beginWord, endWord, wordList)
     print("result is: ", res)
