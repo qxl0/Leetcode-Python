aeb9fda42a2627b14dd8d71a9a799db21b62d81e
Qiang <qxl0@yahoo.com>
2022-04-16 21:10:33 -0400

    Spiral Matrix II

---
+++ SpiralMatrix-II.py
@@ -0,0 +1,30 @@
+"""
+59. Spiral Matrix II
+Medium
+
+3579
+
+180
+
+Add to List
+
+Share
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+ 
+"""
+
+
+import collections
+from typing import List
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 3
+    res = s.generateMatrix(n)
+    print("Ans is:", res)
