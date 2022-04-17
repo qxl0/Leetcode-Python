a8eb7cbdc679d55603c4c9a3c63bd0c0e7481030
Qiang <qxl0@yahoo.com>
2022-04-17 11:49:06 -0400

    Add Binary

---
+++ AddBinary.py
@@ -0,0 +1,32 @@
+"""
+67. Add Binary
+Easy
+
+4857
+
+533
+
+Add to List
+
+Share
+Given two binary strings a and b, return their sum as a binary string.
+ 
+"""
+
+
+import collections
+import sys
+from typing import List
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    a = "11"
+    b = "1"
+    res = sol.addBinary(a, b)
+    print("Ans is:", res)
