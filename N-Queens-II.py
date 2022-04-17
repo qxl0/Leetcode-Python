a00d40af3a69e3bcd27f803575572ffde1aa5ea6
Qiang <qxl0@yahoo.com>
2022-04-16 21:08:43 -0400

    N-Queens II

---
+++ N-Queens-II.py
@@ -0,0 +1,32 @@
+"""
+52. N-Queens II
+Hard
+
+1670
+
+209
+
+Add to List
+
+Share
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+ 
+"""
+
+
+import collections
+from typing import List
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 4
+    res = s.totalNQueens(n)
+    print("Ans is:", res)
