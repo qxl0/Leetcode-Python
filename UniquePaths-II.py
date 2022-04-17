0c3f264d75cf38e0677e18a882b8fc2acbefdb5e
Qiang <qxl0@yahoo.com>
2022-04-17 09:55:30 -0400

    Solved Unique Path II

--- UniquePaths-II.py
+++ UniquePaths-II.py
@@ -26,7 +26,20 @@ from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        pass
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 0:
+                    if i == j == 0:  # dp[0][0]=1
+                        dp[i][j] = 1
+                        continue
+                    else:  # hidden: [-1][j]=0 and dp[i][-1]=0 since dp = [ [0]*n for _ in range(m) ]
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
 
 
 if __name__ == "__main__":
