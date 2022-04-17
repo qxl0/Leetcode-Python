82221fa1d68bd5632347e9b4ad2af4e72adb6fbe
Qiang <qxl0@yahoo.com>
2022-04-17 12:01:35 -0400

    Solved

--- AddBinary.py
+++ AddBinary.py
@@ -21,7 +21,20 @@ from typing import List
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        pass
+        carry = 0
+        result = ""
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            result += str(carry % 2)
+            carry //= 2
+
+        return result[::-1]
 
 
 if __name__ == "__main__":
