6610363df5cce1af729a0ce1ec33a47fc9bb8e17
Qiang <qxl0@yahoo.com>
2022-04-19 14:36:16 -0400

    Solved largest rectangle histogram

--- LargestRectaqngleinHistogram.py
+++ LargestRectaqngleinHistogram.py
@@ -13,19 +13,50 @@ Given an array of integers heights representing the histogram's bar height where
 """
 
 
+import enum
 from typing import List, Optional
 from helpers.LinkedList import Node
 
 
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
+    def largestRectangleArea(self, height: List[int]) -> int:
         """
         Do not return anything, modify nums in-place instead.
         """
+        height.append(0)
+        stack = [-1]
+        ans = 0
+        for i in range(len(height)):
+            while height[i] < height[stack[-1]]:
+                h = height[stack.pop()]
+                w = i - stack[-1] - 1
+                ans = max(ans, h * w)
+            stack.append(i)
+        height.pop()
+        return ans
+
+
+class Solution2:
+    def largestRectangleArea(self, heights):
+        maxArea = 0
+        stack = []  # (start, height)
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append((start, h))
+
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+
+        return maxArea
 
 
 if __name__ == "__main__":
-    sol = Solution()
+    sol = Solution2()
     heights = [2, 1, 5, 6, 2, 3]
     res = sol.largestRectangleArea(heights)
     print(res)
