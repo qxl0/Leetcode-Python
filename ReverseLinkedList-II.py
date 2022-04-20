95d5bb5c3ea5501fd1d9bfd632fe95f96d3ee0a2
Qiang <qxl0@yahoo.com>
2022-04-20 19:49:03 -0400

    Solved reverse linked list II

--- ReverseLinkedList-II.py
+++ ReverseLinkedList-II.py
@@ -18,16 +18,39 @@ from helpers.LinkedList import Node
 
 
 class Solution:
-    def reverseBetween(
-        self, head: Optional[Node], left: int, right: int
-    ) -> Optional[Node]:
+    def reverseBetween(self, head: Optional[Node], l: int, r: int) -> Optional[Node]:
         """
         Do not return anything, modify nums in-place instead.
         """
+        dummy = Node(0)
+        dummy.next = head
+
+        leftPrev, left = dummy, head
+        for _ in range(l - 1):
+            leftPrev, left = left, left.next
+
+        #
+        prev, curr = None, left
+        for i in range(r - l + 1):
+            tmpNext = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmpNext
+
+        # Hook up leftPrev = 1, prev = 4, curr = 5
+        leftPrev.next.next = curr
+        leftPrev.next = prev
+
+        return dummy.next
 
 
 if __name__ == "__main__":
     sol = Solution()
     head = Node(1)
-    res = sol.reverseBetween(head)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+
+    res = sol.reverseBetween(head, 2, 4)
     print(res)
