54f6f8412f417879d195a7e13cb0f83ca3602996
Qiang <qxl0@yahoo.com>
2022-04-20 20:21:46 -0400

    Solved BST gen II

--- UniqueBinarySearchTree-II.py
+++ UniqueBinarySearchTree-II.py
@@ -23,6 +23,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
+        def dfs(start, end):
+            if start == end:
+                return None
+            result = []
+            for i in range(start, end):
+                for l in dfs(start, i) or [None]:
+                    for r in dfs(i + 1, end) or [None]:
+                        node = TreeNode(i)
+                        node.left, node.right = l, r
+                        result.append(node)
+            return result
+
+        if n == 0:
+            return [[]]
+        return dfs(1, n + 1)
+
 
 if __name__ == "__main__":
     sol = Solution()
