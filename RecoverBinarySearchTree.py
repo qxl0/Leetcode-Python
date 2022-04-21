54ac8b495c38d3239e44df1f040f0d3d0cff3b79
Qiang <qxl0@yahoo.com>
2022-04-21 14:07:23 -0400

    Solved by using DFS, inorder

--- RecoverBinarySearchTree.py
+++ RecoverBinarySearchTree.py
@@ -13,6 +13,7 @@ You are given the root of a binary search tree (BST), where the values of exactl
 """
 
 
+import sys
 from typing import List, Optional
 from helpers.TreeNode import TreeNode
 
@@ -23,11 +24,28 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
+        def inorder(node):
+            nonlocal first, second, prev
+            if not node:
+                return
+            inorder(node.left)
+            if not first and prev.val >= node.val:
+                first = prev
+                # print("found first: ", first.val if first else "null")
+            if first and prev.val >= node.val:
+                second = node
+                # print("found second: ", second.val if second else "null")
+            # print("set prev: ", node.val if node else "null")
+            prev = node
+            inorder(node.right)
+
+        first, second, prev = None, None, TreeNode(-sys.maxsize / 2)
+        inorder(root)
+        first.val, second.val = second.val, first.val
+
 
 if __name__ == "__main__":
     sol = Solution()
-    root = TreeNode(1)
-    root.left = TreeNode(3)
-    root.left.right = TreeNode(2)
+    root = TreeNode.to_binary_tree([3, 1, 4, None, None, 2])
     res = sol.recoverTree(root)
     print(res)
