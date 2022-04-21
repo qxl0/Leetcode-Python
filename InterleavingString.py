bad7ce329f51a7313f81f2ff53160f24947bdf2c
Qiang <qxl0@yahoo.com>
2022-04-21 11:12:14 -0400

    Solved Interleaved

--- InterleavingString.py
+++ InterleavingString.py
@@ -30,12 +30,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        dp = {}
+
+        def dfs(i, j):  # s1[i:] s2[j:] is interleave s3[i+j:]
+            if i == len(s1) and j == len(s2) and i + j == len(s3):
+                return True
+            if i + j >= len(s3):
+                dp[(i, j)] = False
+                return False
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+            dp[(i, j)] = False
+            return False
+
+        return dfs(0, 0)
 
 
 if __name__ == "__main__":
     sol = Solution()
-    s1 = "aabcc"
-    s2 = "dbbca"
-    s3 = "aadbbcbcac"
+    # s1 = "aabcc"
+    # s2 = "dbbca"
+    # s3 = "aadbbcbcac"
+    s1 = "a"
+    s2 = "b"
+    s3 = "a"
     res = sol.isInterleave(s1, s2, s3)
     print(res)
