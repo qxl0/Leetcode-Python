13840759ea36d82dcfa96f1b0112067f267101e3
Qiang <qxl0@yahoo.com>
2022-04-22 09:10:53 -0400

    Solved

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -25,11 +25,21 @@ from helpers.TreeNode import TreeNode
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        pass
+        ans = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
 
 
 if __name__ == "__main__":
     sol = Solution()
-    n = 3
+    n = 15
     res = sol.fizzBuzz(n)
     print(res)
