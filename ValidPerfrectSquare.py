c32a69e768680bcd38f8d01f2a2555f483e8318f
Qiang <qxl0@yahoo.com>
2022-04-22 11:39:23 -0400

    Valid perfect square

---
+++ ValidPerfrectSquare.py
@@ -0,0 +1,36 @@
+"""
+367. Valid Perfect Square
+Easy
+
+2188
+
+232
+
+Add to List
+
+Share
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+"""
+import collections
+from typing import List, Optional
+from helpers.TreeNode import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isPerfectSquare(self, num):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    num = 16
+    res = sol.isPerfectSquare(num)
+    print(res)
