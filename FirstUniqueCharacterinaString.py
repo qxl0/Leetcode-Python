8f411384569f36c3ce82088be5065287593476fd
Qiang <qxl0@yahoo.com>
2022-04-22 08:51:26 -0400

    First Unique Character in a string

---
+++ FirstUniqueCharacterinaString.py
@@ -0,0 +1,29 @@
+"""
+387. First Unique Character in a String
+Easy
+
+4864
+
+194
+
+Add to List
+
+Share
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+"""
+
+
+from typing import List, Optional
+from helpers.TreeNode import TreeNode
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "leetcode"
+    res = sol.firstUniqChar(s)
+    print(res)
