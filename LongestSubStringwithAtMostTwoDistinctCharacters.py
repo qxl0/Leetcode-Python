0ec6efd123efc48e649e02e6e927e300fd7b397f
Qiang <qxl0@yahoo.com>
2022-04-23 21:43:46 -0400

    Longest Substring with At Most Two Distinct Characters

---
+++ LongestSubStringwithAtMostTwoDistinctCharacters.py
@@ -0,0 +1,33 @@
+"""
+159. Longest Substring with At Most Two Distinct Characters
+Medium
+
+1685
+
+25
+
+Add to List
+
+Share
+Given a string s, return the length of the longest substring that contains at most two distinct characters.
+"""
+
+
+from typing import List, Optional
+from helpers.LinkedList import LinkedList
+from helpers.LinkedList import ListNode
+from helpers.TreeNode import TreeNode
+
+
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "eceba"
+    res = sol.lengthOfLongestSubstringTwoDistinct(s)
+    print(res)
