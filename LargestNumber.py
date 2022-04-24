be8e2d1e924c3fb53e8487ba81b6c006a4e14556
Qiang <qxl0@yahoo.com>
2022-04-24 14:57:08 -0400

    Largest number

--- LargestNumber.py
+++ LargestNumber.py
@@ -15,6 +15,8 @@ Since the result may be very large, so you need to return a string instead of an
 """
 
 
+from functools import cmp_to_key
+from math import log10
 from typing import List, Optional
 from helpers.LinkedList import LinkedList
 from helpers.LinkedList import ListNode
@@ -27,9 +29,57 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
+        def getSigDigit(n):
+            while n >= 10:
+                n //= 10
+            return n
+
+        def remove_msd(n):
+            if n < 10:
+                return
+            return int(n % (pow(10, log10(n))))
+
+        def comparefn(n1, n2):
+            s1 = getSigDigit(n1)
+            s2 = getSigDigit(n2)
+            if s1 < s2:
+                return 1
+            elif s1 > s2:
+                return -1
+            else:
+                return comparefn(remove_msd(n1), remove_msd(n2))
+
+        a = sorted(nums, key=cmp_to_key(comparefn))
+        print(a)
+
+    def largestNumber2(self, nums):
+        def comparefn(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            elif n1 + n2 < n2 + n1:
+                return 1
+            else:
+                return 0
+
+        if not any(nums):
+            return "0"
+        return "".join(sorted(map(str, nums), key=cmp_to_key(comparefn)))
+
+    def largestNumber3(self, nums):
+        from functools import cmp_to_key
+
+        def mycmp(x, y):
+            return -1 if x + y < y + x else x + y > y + x or 0
+
+        nums = [str(x) for x in nums]
+        nums.sort(key=cmp_to_key(mycmp), reverse=False)
+        return "".join(nums).lstrip("0") or "0"
+
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [10, 2]
-    res = sol.largestNumber(nums)
+    # nums = [10, 2]
+    # nums = [3, 30]
+    nums = [0, 0, 0]
+    res = sol.largestNumber3(nums)
     print(res)
