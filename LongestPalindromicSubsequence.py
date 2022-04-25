0357269757cc881d3030adc89377de7ca5dd2030
Qiang <qxl0@yahoo.com>
2022-04-25 10:26:57 -0400

    Longest Palindromic Subsequence

---
+++ LongestPalindromicSubsequence.py
@@ -0,0 +1,32 @@
+"""
+516. Longest Palindromic Subsequence
+Medium
+
+5073
+
+246
+
+Add to List
+
+Share
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+"""
+from collections import defaultdict
+import collections
+from math import factorial
+from operator import itemgetter
+from typing import List
+
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1, 2, 3, 4, 5]
+    res = sol.increasingTriplet(nums)
+    print(res)
