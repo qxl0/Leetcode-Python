c1f173c6e36ccf28f1e6016c779fd80be246463a
Qiang <qxl0@yahoo.com>
2022-04-25 20:56:22 -0400

    Solved Samllest subsequence distinct characters

--- SmallestSubsequenceofDistinctCharacters.py
+++ SmallestSubsequenceofDistinctCharacters.py
@@ -20,7 +20,15 @@ from typing import List
 
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
-        pass
+        last = {c: i for i, c in enumerate(s)}
+        stack = []
+        for i, c in enumerate(s):
+            if c in stack:
+                continue
+            while stack and stack[-1] > c and i < last[stack[-1]]:
+                stack.pop()
+            stack.append(c)
+        return "".join(stack)
 
 
 if __name__ == "__main__":
