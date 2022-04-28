56914af1b8207d4c8a5fbbd46b27295ea568eb51
Qiang <qxl0@yahoo.com>
2022-04-28 10:52:52 -0400

    Car Fleet

---
+++ CarFleet.py
@@ -0,0 +1,44 @@
+"""
+853. Car Fleet
+Medium
+
+1354
+
+402
+
+Add to List
+
+Share
+There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+
+You are given two integer array position and speed, both of length n, where position[i] is the position of the ith car and speed[i] is the speed of the ith car (in miles per hour).
+
+A car can never pass another car ahead of it, but it can catch up to it and drive bumper to bumper at the same speed. The faster car will slow down to match the slower car's speed. The distance between these two cars is ignored (i.e., they are assumed to have the same position).
+
+A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car is also a car fleet.
+
+If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+
+Return the number of car fleets that will arrive at the destination.
+"""
+
+
+from math import floor
+from re import S
+from typing import List, Optional
+from helpers.LinkedList import LinkedList, ListNode
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    target = 12
+
+    position = [10, 8, 0, 5, 3]
+    speed = [2, 4, 1, 1, 3]
+    res = sol.carFleet(target, position, speed)
+    print(res)
