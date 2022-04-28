55e0495c18729ff26c71018df74a0c0f52b58def
Qiang <qxl0@yahoo.com>
2022-04-27 21:04:12 -0400

    Reconstruct Original Digits from English

---
+++ ReconstructOriginalDigitsFromEnglish.py
@@ -0,0 +1,29 @@
+"""
+423. Reconstruct Original Digits from English
+Medium
+
+606
+
+2051
+
+Add to List
+
+Share
+Given a string s containing an out-of-order English representation of digits 0-9, return the digits in ascending order.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Solution:
+    def originalDigits(self, s: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "owoztneoer"
+    res = sol.originalDigits(s)
+    print(res)
