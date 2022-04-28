9477625a2a9d45b0c9bc192350fa3b66d7a6c26b
Qiang <qxl0@yahoo.com>
2022-04-27 21:14:33 -0400

    Integer to English Words

---
+++ IntegertoEnglishWords.py
@@ -0,0 +1,30 @@
+"""
+273. Integer to English Words
+Hard
+
+2050
+
+4986
+
+Add to List
+
+Share
+Convert a non-negative integer num to its English words representation.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "123"
+    res = sol.numberToWords(s)
+
+    print(res)
