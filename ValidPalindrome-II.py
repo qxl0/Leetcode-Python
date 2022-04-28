3875dd2b3fa5e97794aa1ab7f70103a0bc9d5f2f
Qiang <qxl0@yahoo.com>
2022-04-27 21:05:35 -0400

    Valid Parlindrome II

---
+++ ValidPalindrome-II.py
@@ -0,0 +1,29 @@
+"""
+680. Valid Palindrome II
+Easy
+
+5541
+
+300
+
+Add to List
+
+Share
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Solution:
+    def validPalindrome(self, s: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "aba"
+    res = sol.validPalindrome(s)
+    print(res)
