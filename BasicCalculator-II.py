592091bff45368b1582697ff029a742e6dfa7845
Qiang <qxl0@yahoo.com>
2022-04-29 09:59:37 -0400

    Solved basic calculator II

--- BasicCalculator-II.py
+++ BasicCalculator-II.py
@@ -25,11 +25,46 @@ from typing import List
 
 class Solution:
     def calculate(self, s: str) -> int:
-        pass
+        num, stack, sign = 0, [], "+"
+        for i in range(len(s)):
+            if s[i].isdigit():
+                num = num * 10 + int(s[i])
+            if s[i] in "+-*/" or i == len(s) - 1:
+                if sign == "+":
+                    stack.append(num)
+                elif sign == "-":
+                    stack.append(-num)
+                elif sign == "*":
+                    stack.append(stack.pop() * num)
+                else:
+                    stack.append(int(stack.pop() / num))
+                num = 0
+                sign = s[i]
+        return sum(stack)
+
+
+class Solution2:
+    def calculate(self, s):
+        val, stack, op = 0, [], "+"
+        for i, c in enumerate(s):
+            if c.isdigit():
+                val = val * 10 + int(c)
+            if c in "+-*/" or i == len(s) - 1:
+                if op == "+":
+                    stack.append(val)
+                elif op == "-":
+                    stack.append(-val)
+                elif op == "*":
+                    stack.append(stack.pop() * val)
+                elif op == "/":
+                    stack.append(int(stack.pop() / val))
+                val = 0
+                op = c
+        return sum(stack)
 
 
 if __name__ == "__main__":
-    sol = Solution()
-    s = "3+2*2"
+    sol = Solution2()
+    s = "-3-2*2"
     res = sol.calculate(s)
     print(res)
