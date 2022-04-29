6fcf414c971ba5790a6358d5ea2319fda278ae83
Qiang <qxl0@yahoo.com>
2022-04-29 13:15:42 -0400

    Solved

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -21,11 +21,36 @@ from typing import List
 
 class Solution:
     def calculate(self, s: str) -> int:
-        pass
+        stack = []
+        operand = 0
+        res = 0  # For the on-going result
+        sign = 1  # 1 means positive, -1 means negative
+        for ch in s:
+            if ch.isdigit():
+                operand = (operand * 10) + int(ch)
+            elif ch in "+-":
+                res += sign * operand
+                operand = 0
+                if ch == "-":
+                    sign = -1
+                else:
+                    sign = 1
+            elif ch == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign = 1
+                res = 0
+            elif ch == ")":
+                res += sign * operand
+                res *= stack.pop()  # stack pop 1, sign
+                res += stack.pop()  # stack pop 2, operand
+                operand = 0
+        return res + sign * operand
 
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "-3-2*2"
+    # s = "3-(2+(9-4))"
+    s = "(1+(4+5+2)-3)+(6+8)"
     res = sol.calculate(s)
     print(res)
