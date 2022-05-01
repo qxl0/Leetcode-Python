a2574cdcd9f72ef9bdfdf81238d66e69a2ee8bf8
Qiang <qxl0@yahoo.com>
2022-05-01 11:33:37 -0400

    Next Greater Element III

---
+++ NexstGreaterElement-III.py
@@ -0,0 +1,28 @@
+"""
+556. Next Greater Element III
+Medium
+
+2198
+
+346
+
+Add to List
+
+Share
+Given a positive integer n, find the smallest integer which has exactly the same digits existing in the integer n and is greater in value than n. If no such positive integer exists, return -1.
+
+Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it does not fit in 32-bit integer, return -1.
+"""
+
+
+class Solution:
+    def nextGreaterElements(self, n):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    n = 12
+    res = sol.nextGreaterElements(n)
+
+    print(res)
