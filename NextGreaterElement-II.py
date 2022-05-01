6fe7745fcc9cb48b67693b7ae4939736ec73ac6e
Qiang <qxl0@yahoo.com>
2022-05-01 10:45:49 -0400

    Next Greater Element II

---
+++ NextGreaterElement-II.py
@@ -0,0 +1,33 @@
+"""
+503. Next Greater Element II
+Medium
+
+4475
+
+132
+
+Add to List
+
+Share
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+"""
+
+
+class Solution:
+    def nextGreaterElements(self, A):
+        stack, res = [], [-1] * len(A)
+        for i in range(len(A)) * 2:
+            while stack and (A[stack[-1]] < A[i]):
+                res[stack.pop()] = A[i]
+            stack.append(i)
+        return res
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1, 2, 1]
+    res = sol.nextGreaterElements(nums)
+
+    print(res)
