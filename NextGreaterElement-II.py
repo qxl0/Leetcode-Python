9ad4fee2a59f9102a25fb136d5ddb125493a6565
Qiang <qxl0@yahoo.com>
2022-05-01 10:53:13 -0400

    Solved by using stack, and perform twice

--- NextGreaterElement-II.py
+++ NextGreaterElement-II.py
@@ -18,7 +18,11 @@ The next greater number of a number x is the first greater number to its travers
 class Solution:
     def nextGreaterElements(self, A):
         stack, res = [], [-1] * len(A)
-        for i in range(len(A)) * 2:
+        for i in range(len(A)):
+            while stack and (A[stack[-1]] < A[i]):
+                res[stack.pop()] = A[i]
+            stack.append(i)
+        for i in range(len(A)):
             while stack and (A[stack[-1]] < A[i]):
                 res[stack.pop()] = A[i]
             stack.append(i)
