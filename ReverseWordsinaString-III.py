eb71d10c52bcebd35ac42531659dcb88085350fc
Qiang <qxl0@yahoo.com>
2022-05-02 15:57:31 -0400

    Reverse words in a string

---
+++ ReverseWordsinaString-III.py
@@ -0,0 +1,26 @@
+"""
+557. Reverse Words in a String III
+Easy
+
+2746
+
+163
+
+Add to List
+
+Share
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+"""
+from string import ascii_lowercase, ascii_uppercase
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "Let's take LeetCode contest"
+    res = sol.reverseWords(s)
+    print(res)
