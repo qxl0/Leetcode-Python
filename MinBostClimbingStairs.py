46193e5864ddb4317c44194136a991d5b8bd893a
Qiang <qxl0@yahoo.com>
2022-05-03 06:57:27 -0400

    Solved using DP

--- MinBostClimbingStairs.py
+++ MinBostClimbingStairs.py
@@ -29,7 +29,12 @@ from helpers.TreeNode import TreeNode
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        pass
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1])
+        print(dp)
+        return dp[-1]
 
 
 if __name__ == "__main__":
