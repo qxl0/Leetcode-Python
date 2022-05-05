08ddd50aa266a65aa4f79a262ce3551e75f40583
Qiang <qxl0@yahoo.com>
2022-05-04 21:15:14 -0400

    Employee free time

---
+++ EmployeeFreeTime.py
@@ -0,0 +1,42 @@
+"""
+759. Employee Free Time
+Hard
+
+1398
+
+87
+
+Add to List
+
+Share
+We are given a list schedule of employees, which represents the working time for each employee.
+
+Each employee has a list of non-overlapping Intervals, and these intervals are in sorted order.
+
+Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
+
+(Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays. For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined).  Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+
+
+class Solution:
+    def employeeFreeTime(self, schedule):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    schedule = [[[1, 2], [5, 6]], [[1, 3]], [[4, 10]]]
+    res = sol.employeeFreeTime(schedule)
+
+    print(res)
