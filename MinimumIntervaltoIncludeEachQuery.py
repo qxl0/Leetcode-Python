3111c5552a88e90116cd36cd64a26f6cd987fbf2
Qiang <qxl0@yahoo.com>
2022-05-05 21:03:43 -0400

    Solved

--- MinimumIntervaltoIncludeEachQuery.py
+++ MinimumIntervaltoIncludeEachQuery.py
@@ -19,13 +19,25 @@ Return an array containing the answers to the queries.
 
 from calendar import c
 import collections
+import heapq
 from math import floor
 from typing import List
 
 
 class Solution:
-    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        pass
+    def minInterval(self, A, queries):
+        A = sorted(A)[::-1]
+        h = []
+        res = {}
+        for q in sorted(queries):
+            while A and A[-1][0] <= q:
+                i, j = A.pop()
+                if j >= q:
+                    heapq.heappush(h, [j - i + 1, j])
+            while h and h[0][1] < q:
+                heapq.heappop(h)
+            res[q] = h[0][0] if h else -1
+        return [res[q] for q in queries]
 
 
 if __name__ == "__main__":
