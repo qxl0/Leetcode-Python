4f62267469b0e17687b4170c741f6ef19a7656bc
Qiang <qxl0@yahoo.com>
2022-05-06 19:30:43 -0400

    Super Pow

---
+++ SuperPow.py
@@ -0,0 +1,30 @@
+"""
+372. Super Pow
+Medium
+
+469
+
+1091
+
+Add to List
+
+Share
+Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    a = 2
+    b = [3]
+    res = sol.superPow(a, b)
+    print(res)
