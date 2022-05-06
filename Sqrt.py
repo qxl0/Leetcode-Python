368dff4ccf0ed1f5e1684fe0c0366ce551d0d935
Qiang <qxl0@yahoo.com>
2022-05-06 19:09:13 -0400

    Sqrt

---
+++ Sqrt.py
@@ -0,0 +1,33 @@
+"""
+69. Sqrt(x)
+Easy
+
+3760
+
+3185
+
+Add to List
+
+Share
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    x = 4
+    res = sol.mySqrt(x)
+    print(res)
