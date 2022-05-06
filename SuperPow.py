bb86dd3869ba20046deab0c4585ee63fc050c28c
Qiang <qxl0@yahoo.com>
2022-05-06 19:42:43 -0400

    Convert to int then call pow()

--- SuperPow.py
+++ SuperPow.py
@@ -16,15 +16,18 @@ Your task is to calculate ab mod 1337 where a is a positive integer and b is an 
 from math import floor
 from typing import List
 
+MOD = 1337
+
 
 class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
-        pass
+        b = int("".join(map(str, b)))
+        return pow(a, b, MOD)
 
 
 if __name__ == "__main__":
     sol = Solution()
     a = 2
-    b = [3]
+    b = [1, 0]
     res = sol.superPow(a, b)
     print(res)
