9c168cdf911a35bd16c3862f6898ee201c12c867
Qiang <qxl0@yahoo.com>
2022-05-07 16:25:43 -0400

    Solved Container Duplciate III using Bucket Sort

--- ContainsDuplicates-III.py
+++ ContainsDuplicates-III.py
@@ -17,14 +17,46 @@ from typing import List
 
 
 class Solution:
-    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int) -> bool:
-        pass
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        def getID(x, w):
+            return x // w
+
+        if t < 0:
+            return False
+        d = {}
+        w = t + 1
+        for i in range(len(nums)):
+            m = getID(nums[i], w)
+            if m in d:
+                return True
+            if m - 1 in d and abs(nums[i] - d[m - 1]) < w:
+                return True
+            if m + 1 in d and abs(nums[i] - d[m + 1]) < w:
+                return True
+            d[m] = nums[i]
+            if i >= k:
+                d.pop(getID(nums[i - k], w))
+        return False
 
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [1, 2, 3, 1]
-    k = 3
-    t = 0
+    # nums = [8, 7, 15, 1, 6, 1, 9, 15]
+    # k = 1
+    # t = 3
+    # nums = [1, 2, 3, 1]
+    # k = 3
+    # t = 0
+    nums = [-3, 3, -6]
+    k = 2
+    t = 3
+    res = sol.containsNearbyAlmostDuplicate(nums, k, t)
+    nums = [1, 0, 1, 1]
+    k = 1
+    t = 2
+    res = sol.containsNearbyAlmostDuplicate(nums, k, t)
+    nums = [1, 5, 9, 1, 5, 9]
+    k = 2
+    t = 3
     res = sol.containsNearbyAlmostDuplicate(nums, k, t)
     print("Ans is: ", res)
