df6e46ddb0271abf15ba78ebc1733fbfa6c3e8a5
Qiang <qxl0@yahoo.com>
2022-05-07 10:47:49 -0400

    Solved DetectSquares

--- DetectSquares.py
+++ DetectSquares.py
@@ -13,6 +13,7 @@ Your task is to calculate ab mod 1337 where a is a positive integer and b is an 
 """
 
 
+from collections import Counter
 from math import floor
 from typing import List
 
@@ -21,19 +22,26 @@ MOD = 1337
 
 class DetectSquares:
     def __init__(self):
-        pass
+        self.d = Counter()
 
     def add(self, point: List[int]) -> None:
-        pass
+        self.d[tuple(point)] += 1
 
     def count(self, point: List[int]) -> int:
-        pass
+        ans = 0
+        x1, y1 = point
+        for p3, cnt in self.d.items():
+            x3, y3 = p3
+            if x1 == x3 or abs(x1 - x3) != abs(y1 - y3):
+                continue
+            ans += cnt * self.d[(x1, y3)] * self.d[(x3, y1)]
+        return ans
 
 
 if __name__ == "__main__":
     sol = DetectSquares()
-    sol.add([[3, 10]])
-    sol.add([[11, 2]])
-    sol.add([[3, 2]])
-    res = sol.count([[11, 10]])
+    sol.add([3, 10])
+    sol.add([11, 2])
+    sol.add([3, 2])
+    res = sol.count([11, 10])
     print(res)
