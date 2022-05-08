83616325cf51665851ce456772d21ad0461b88c1
Qiang <qxl0@yahoo.com>
2022-05-08 14:03:18 -0400

    Maximum product of three numbers

--- MaximumProductSubarray.py
+++ MaximumProductSubarray.py
@@ -1,3 +1,20 @@
+"""
+152. Maximum Product Subarray
+Medium
+
+11364
+
+350
+
+Add to List
+
+Share
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+A subarray is a contiguous subsequence of the array.
+"""
 import sys
 from typing import List
 
---
+++ MaximumProdutofThreeNumbers.py
@@ -0,0 +1,27 @@
+"""
+628. Maximum Product of Three Numbers
+Easy
+
+2678
+
+524
+
+Add to List
+
+Share
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+"""
+import sys
+from typing import List
+
+
+class Solution:
+    def maximumProduct(nums):
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [-4, -3, -2]
+    res = s.maximumProduct(nums)
+    print(res)
