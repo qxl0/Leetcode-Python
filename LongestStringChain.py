961ba882c357f30b8ec0ed7f4d4f1c16096941f0
Qiang <qxl0@yahoo.com>
2022-05-09 15:51:34 -0400

    Solved using DFS

--- LongestStringChain.py
+++ LongestStringChain.py
@@ -30,11 +30,31 @@ from helpers.TreeNode import TreeNode
 
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        pass
+        wordset = set(words)
+        memo = {}
+        longest = 0
+
+        def helper(word, memo):
+            nonlocal longest
+            if word in memo:
+                return memo[word]
+            length = 1
+            for i in range(len(word)):
+                newword = word[:i] + word[i + 1 :]
+                if newword in wordset:
+                    length = max(length, 1 + helper(newword, memo))
+            longest = max(longest, length)
+            memo[word] = length
+            return memo[word]
+
+        for word in words:
+            helper(word, memo)
+        return longest
 
 
 if __name__ == "__main__":
     sol = Solution()
-    words = ["a", "b", "ba", "bca", "bda", "bdca"]
+    # words = ["a", "b", "ba", "bca", "bda", "bdca"]
+    words = ["a", "ba"]
     res = sol.longestStrChain(words)
     print("result is: ", res)
