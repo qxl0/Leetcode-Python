515a6619c856c2a81a5eb88fc0407cc79789a9c1
Qiang <qxl0@yahoo.com>
2022-05-13 19:26:06 -0400

    Recursive solution

--- DecodeString.py
+++ DecodeString.py
@@ -25,12 +25,64 @@ from typing import List
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        pass
+        stack = []
+
+        def decode():
+            s = []
+            while stack and stack[-1] != "[":
+                s.append(stack.pop())
+            s = s[::-1]
+            stack.pop()
+            num = []
+            while stack and stack[-1].isdigit():
+                num.append((stack.pop()))
+            num = num[::-1]
+            t = int("".join(num))
+            stack.append("".join(s) * t)
+
+        for ch in s:
+            if ch == "]":
+                decode()
+                continue
+            stack.append(ch)
+        return "".join(stack)
+
+
+class Solution2:
+    def decodeString(self, s):
+        if not s or len(s) == 0:
+            return s
+        result, position = self.dfs(0, s, 0, "")
+        return result
+
+    def dfs(self, position, s, prev_num, prev_str):
+        while position < len(s):
+            while s[position].isdigit():
+                prev_num = prev_num * 10 + int(s[position])
+                position += 1
+
+            if s[position] == "[":
+                # reset the prev_str
+                returned_str, ending_pos = self.dfs(
+                    position + 1, s, prev_num=0, prev_str=""
+                )
+                # backtrack
+                prev_str = prev_str + returned_str * prev_num
+                position = ending_pos
+                prev_num = 0
+            # return the result
+            elif s[position] == "]":
+                return prev_str, position
+            else:
+                prev_str += s[position]
+            position += 1
+        return prev_str, position
 
 
 if __name__ == "__main__":
-    sol = Solution()
-    s = "2[a]2[bc]"
+    sol = Solution2()
+    # s = "2[a]2[bc]"
+    s = "2[4[2[jk]ef]]"
     res = sol.decodeString(s)
 
     print(res)
