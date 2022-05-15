2cd1d20480a51d85a7dbc61052079b5a5d7cae26
Qiang <qxl0@yahoo.com>
2022-05-15 11:32:03 -0400

    Use Preorder[:i] as stack to achieve O(1) space

--- VerifyPreOrderSequenceinBinarySearchTree.py
+++ VerifyPreOrderSequenceinBinarySearchTree.py
@@ -32,6 +32,22 @@ class Solution:
         return True
 
 
+class Solution2:
+    def verifyPreorder(self, preorder):
+        # stack = preorder[:i], reuse preorder as stack
+        lower = -1 << 31
+        i = 0
+        for x in preorder:
+            if x < lower:
+                return False
+            while i > 0 and x > preorder[i - 1]:
+                lower = preorder[i - 1]
+                i -= 1
+            preorder[i] = x
+            i += 1
+        return True
+
+
 if __name__ == "__main__":
     sol = Solution()
     nums = [5, 2, 1, 3, 6]
