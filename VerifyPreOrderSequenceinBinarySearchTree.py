6c6ecaf50b08f7c46e40eec5d9d08722eac93b94
Qiang <qxl0@yahoo.com>
2022-05-15 10:26:19 -0400

    Pre order sequence in BST

---
+++ VerifyPreOrderSequenceinBinarySearchTree.py
@@ -0,0 +1,32 @@
+"""
+## (**255) Verify Preorder Sequence in Binary Search Tree**
+
+**Medium**
+
+927
+
+70
+
+Add to List
+
+Share
+
+Given an array of **unique** integers `preorder`, return `true` *if it is the correct preorder traversal sequence of a binary search tree*.
+"""
+
+
+from math import floor
+from typing import List
+
+
+class Solution:
+    def verifyPreorder(self, preorder):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [5, 2, 1, 3, 6]
+    res = sol.verifyPreorder(nums)
+
+    print(res)
