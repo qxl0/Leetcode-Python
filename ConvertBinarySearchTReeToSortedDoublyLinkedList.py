480c0dda785445a1f0bec70bae3d7087517ca519
Qiang <qxl0@yahoo.com>
2022-05-17 14:25:23 -0400

    Solved using Recursion

--- ConvertBinarySearchTReeToSortedDoublyLinkedList.py
+++ ConvertBinarySearchTReeToSortedDoublyLinkedList.py
@@ -28,9 +28,39 @@ class Solution:
         return first
 
 
+class Solution2:
+    def treeToDoublyList(self, root: "Optional[Node]") -> "Optional[Node]":
+        def helper(node):
+            if not node:
+                return (None, None)
+            curfirst = node
+            if node.left:
+                lfirst, llast = helper(node.left)
+                node.left = llast
+                llast.right = node
+                curfirst = lfirst
+            curlast = node
+            if node.right:
+                rfirst, rlast = helper(node.right)
+                node.right = rfirst
+                rfirst.left = node
+                curlast = rlast
+            # print(node.val,":  ",curfirst.val,curlast.val)
+            return (curfirst, curlast)
+
+        if not root:
+            return None
+        first, last = helper(root)
+        if first:
+            first.left = last
+        if last:
+            last.right = first
+        return first
+
+
 if __name__ == "__main__":
     sol = Solution()
     root = TreeNode.to_binary_tree([5, 3, 6, 2, 4, None, None, 1])
-    res = sol.findLeaves(root)
+    res = sol.treeToDoublyList(root)
 
     print(res)
