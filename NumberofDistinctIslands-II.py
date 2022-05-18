442ae4d4a1e3d0ed4a768cc3ca8fc486b948184f
Qiang <qxl0@yahoo.com>
2022-05-18 12:28:41 -0400

    NUmber of Distinct Islands II

---
+++ NumberofDistinctIslands-II.py
@@ -0,0 +1,74 @@
+class Solution:
+    def numDistinctIslands2(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        shapes, vis = set(), set()
+
+        def helper(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n:
+                return
+            if grid[r][c] != 1 or (r, c) in vis:
+                return
+            vis.add((r, c))
+            shape.append((r, c))
+            helper(r + 1, c)
+            helper(r - 1, c)
+            helper(r, c + 1)
+            helper(r, c - 1)
+
+        def canonical(shape):
+            res = ""
+            xs = [0] * len(shape)
+            ys = [0] * len(shape)
+            out = [0] * len(shape)
+            for c in range(8):
+                t = 0
+                for x, y in shape:
+                    if c <= 1:
+                        xs[t] = x
+                    elif c <= 3:
+                        xs[t] = -x
+                    elif c <= 5:
+                        xs[t] = y
+                    else:
+                        xs[t] = -y
+                    if c <= 3:
+                        if c % 2 == 0:
+                            ys[t] = y
+                        else:
+                            ys[t] = -y
+                    else:
+                        if c % 2 == 0:
+                            ys[t] = x
+                        else:
+                            ys[t] = -x
+                    t += 1
+                mx, my = xs[0], ys[0]
+
+                mx = min(xs)
+                my = min(ys)
+
+                out = []
+                for j in range(len(shape)):
+                    out.append(str((xs[j] - mx) * n + (ys[j] - my)))
+
+                out.sort()
+                candidate = "".join(out)
+                if res > candidate:
+                    res = candidate
+            return res
+
+        for i in range(m):
+            for j in range(n):
+                shape = []
+                helper(i, j)
+                if shape:
+                    shapes.add(canonical(shape))
+        return len(shapes)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    grid = [[1, 1, 0, 1, 1], [1, 0, 0, 0, 0], [0, 0, 0, 0, 1], [1, 1, 0, 1, 1]]
+    res = sol.numDistinctIslands2(grid)
+
+    print(res)
