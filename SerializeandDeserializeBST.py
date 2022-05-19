0a57a43cc17c18b392ef4053745a4100ef5263cb
Qiang <qxl0@yahoo.com>
2022-05-18 20:51:37 -0400

    Fixed a bug

--- SerializeandDeserializeBST.py
+++ SerializeandDeserializeBST.py
@@ -17,8 +17,9 @@ class Codec:
         """Decodes your encoded data to tree."""
 
         def helper(lower=float("-inf"), upper=float("inf")):
-            if not data or data[-1] < lower or data[-1] < upper:
+            if not data or data[-1] < lower or data[-1] > upper:
                 return None
+
             val = data.pop()
             root = TreeNode(val)
             root.right = helper(val, upper)
