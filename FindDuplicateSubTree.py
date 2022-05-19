81994fad2ab14d7638566ff4f329af169cc14108
Qiang <qxl0@yahoo.com>
2022-05-18 20:07:24 -0400

    Another solution

--- FindDuplicateSubTree.py
+++ FindDuplicateSubTree.py
@@ -17,11 +17,43 @@ class Solution:
         return [nodes[struct][0] for struct in nodes if len(nodes[struct]) > 1]
 
 
+class Solution2:
+    def findDuplicateSubtrees(
+        self, root: Optional[TreeNode]
+    ) -> List[Optional[TreeNode]]:
+        key2Id, key2count = {}, {}
+        res = []
+
+        def getId(node):
+            if not node:
+                return -1
+
+            key = (
+                str(node.val)
+                + "#"
+                + str(getId(node.left))
+                + "#"
+                + str(getId(node.right))
+            )
+            if key not in key2Id:
+                key2Id[key] = len(key2Id)
+                key2count[key] = 1
+            else:
+                key2count[key] += 1
+                if key2count[key] == 2:
+                    res.append(node)
+
+            return key2Id[key]
+
+        getId(root)
+        return res
+
+
 if __name__ == "__main__":
     sol = Solution()
     s = [1, 2, 3, 4, None, 2, 4, None, None, 4]
     root = TreeNode.to_binary_tree(s)
     p = 5
     q = 4
-    res = sol.lowestCommonAncestor(root, p, q)
+    res = sol.findDuplicateSubtrees(root)
     print("Ans is: ", res)
