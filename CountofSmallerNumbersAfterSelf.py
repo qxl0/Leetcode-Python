b7c249a3c2af4a083d9f28117e029329c841fc65
Qiang <qxl0@yahoo.com>
2022-05-23 19:28:49 -0400

    Count smaller items  after self

--- CountofSmallerNumbersAfterSelf.py
+++ CountofSmallerNumbersAfterSelf.py
@@ -24,29 +24,40 @@ from helpers.TreeNode import TreeNode
 
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        count = [0] * n
-        snums = nums.copy()
+        def query(left, right, tree, size):
+            # tree: [.....] (20002)
+            # return [left,right)
+            res = 0
+            left += size
+            right += size  # leaf
+            while left < right:
+                if left % 2 == 1:
+                    res += tree[left]
+                    left += 1
+                left //= 2
+                if right % 2 == 1:
+                    right -= 1
+                    res += tree[right]
+                right //= 2
+            return res
 
-        def helper(nums, l, r):
-            nonlocal count, snums
-            if l >= r:
-                return
+        def update(index, value, tree, size):
+            index += size
+            tree[index] += value  # add on top of
+            # now update parent
+            while index > 1:
+                index //= 2
+                tree[index] = tree[index * 2] + tree[index * 2 + 1]
 
-            mid = l + (r - l) // 2
-
-            helper(nums, l, mid)
-            helper(nums, mid + 1, r)
-
-            for i in range(l, mid + 1):
-                pos = bisect_left(snums, nums[i], mid + 1, r + 1)
-                count[i] += pos - (mid + 1)
-
-            snums = snums[:l] + sorted(snums[l : r + 1]) + snums[r + 1 :]
-            print(snums)
-
-        helper(nums, 0, n - 1)
-        return count
+        offset = 10**4
+        size = 2 * 10**4 + 1
+        tree = [0] * 2 * size
+        result = []
+        for num in nums[::-1]:
+            cnt = query(0, num + offset, tree, size)
+            result.append(cnt)
+            update(num + offset, 1, tree, size)
+        return result[::-1]
 
 
 class Solution2:
@@ -76,19 +87,20 @@ class Solution2:
                 index //= 2
                 tree[index] = tree[index * 2] + tree[index * 2 + 1]
 
-        offset = 10**4
-        size = 2 * 10**4 + 1
-        tree = [0] * 2 * size
+        N = max(nums)
+        size = N
+        tree = [0] * (2 * size + 2)
         result = []
         for num in nums[::-1]:
-            cnt = query(0, num + offset, tree, size)
+            cnt = query(0, num, tree, size)
             result.append(cnt)
-            update(num + offset, 1, tree, size)
+            update(num, 1, tree, size)
         return result[::-1]
 
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [5, 2, 6, 1]
+    nums = [4, 2, 5, 6]
+    # nums = [-1, -1]
     res = sol.countSmaller(nums)
     print(res)
