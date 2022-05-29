1d3f972d788d7e49d85da4cc5c04497160e3dd22
Qiang <qxl0@yahoo.com>
2022-05-29 14:29:41 -0400

    Different ways to add parentheses

---
+++ DifferentWaystoAddParentheses.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    res = sol.maxProduct(s)
+    print("Ans is: ", res)
