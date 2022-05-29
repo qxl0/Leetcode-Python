c71b6d7f387fcb5c19a869af47f4e119af26c42e
Qiang <qxl0@yahoo.com>
2022-05-29 14:31:01 -0400

    Maximum Gap

---
+++ MaximumGap.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    res = sol.maxProduct(s)
+    print("Ans is: ", res)
