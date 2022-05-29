909bc2d02621e253f9d6b7d7f651ed658a9fe13f
Qiang <qxl0@yahoo.com>
2022-05-29 11:32:36 -0400

    Leetcode 1960

---
+++ MaximumProductoftheLengthofTwoPalindromicSubstrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, s: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    res = sol.maxProduct(s)
+    print("Ans is: ", res)
