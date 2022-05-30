73ff59d8ccc32c072f1ab4abb219f656939652cf
Qiang <qxl0@yahoo.com>
2022-05-30 10:46:04 -0400

    Solved

--- PaintHouse.py
+++ PaintHouse.py
@@ -3,7 +3,28 @@ from typing import List
 
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        pass
+        n = len(costs)
+        memo = {}
+
+        def helper(cur, color):
+            if (cur, color) in memo:
+                return memo[(cur, color)]
+            total_cost = costs[cur][color]
+            if cur == n - 1:
+                pass
+            elif color == 0:
+                total_cost += min(helper(n + 1, 1), helper(n + 1, 2))
+            elif color == 1:
+                total_cost += min(helper(n + 1, 0), helper(n + 1, 1))
+            else:
+                total_cost += min(helper(n + 1, 0), helper(n + 1, 1))
+
+            memo[(cur, color)] = total_cost
+            return total_cost
+
+        if not costs:
+            return 0
+        return min(helper(0, 0), helper(0, 1), helper(0, 2))
 
 
 if __name__ == "__main__":
