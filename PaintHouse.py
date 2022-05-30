9b392e2b598f9ed842e28b6d8ec881d4a611678d
Qiang <qxl0@yahoo.com>
2022-05-30 09:30:09 -0400

    Paint House

---
+++ PaintHouse.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    costs = [[17, 2, 17], [16, 16, 5], [14, 3, 19]]
+    res = sol.minCost(costs)
+    print("Ans is: ", res)
