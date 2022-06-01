c99b5ddcd72a8b41a9439247cfd3c496de987d3b
Qiang <qxl0@yahoo.com>
2022-05-31 22:14:24 -0400

    h index

---
+++ H-Index.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def hIndex(self, citations):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    citations = [1, 3, 3, 5, 100]
+    res = sol.hIndex(citations)
+    print(res)
