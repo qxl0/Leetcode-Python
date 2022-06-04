bd6e985a4d3561e48668ed017db8bbbe59e3b0b3
Qiang <qxl0@yahoo.com>
2022-06-04 14:24:16 -0400

    Solved a bug

--- helpers/AdditiveNumber.py
+++ helpers/AdditiveNumber.py
@@ -2,25 +2,38 @@ class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
         n = len(num)
         ans = []
+        isAdd = False
 
         def dfs(i):
-            print(ans)
-            if i == n:
+            nonlocal isAdd
+            if isAdd:
                 return
-            for j in range(i + 1, n):
+            if i == n:
+                if len(ans) > 2:
+                    isAdd = True
+                    return
+            for j in range(i + 1, n + 1):
+                if isAdd:
+                    return
                 cur = int(num[i:j])
                 if len(num[i:j]) >= 2 and num[i] == "0":
                     continue
-                if len(ans) >= 2 and ans[-1] + ans[-2] == cur:
+                if len(ans) < 2:
                     ans.append(cur)
-                    dfs(j + 1)
+                    dfs(j)
+                    ans.pop()
+                elif ans[-1] + ans[-2] == cur:
+                    ans.append(cur)
+                    dfs(j)
+                    ans.pop()
 
         dfs(0)
-        return len(ans) == n
+        return isAdd
 
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "112358"
+    # s = "112358"
+    s = "111"
     res = sol.isAdditiveNumber(s)
     print(res)
