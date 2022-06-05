be6d60624b9e1b69ef83773b6566799bc30f02ff
Qiang <qxl0@yahoo.com>
2022-06-05 10:23:04 -0400

    recursive solution

--- helpers/RemoveDuplicateLetters.py
+++ helpers/RemoveDuplicateLetters.py
@@ -1,14 +1,27 @@
+from collections import Counter
 import heapq
 from typing import List
 
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        pass
+        c = Counter(s)
+        pos = 0
+        for i in range(len(s)):
+            if s[i] < s[pos]:
+                pos = i
+            c[s[i]] -= 1
+            if c[s[i]] == 0:
+                break
+        return (
+            s[pos] + self.removeDuplicateLetters(s[pos:].replace(s[pos], ""))
+            if s
+            else ""
+        )
 
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "cbacdcbc"
+    s = "cbacdcbca"
     res = sol.removeDuplicateLetters(s)
     print(res)
