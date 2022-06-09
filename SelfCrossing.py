e3e1e04a40313cc55d93d0bbe6b129934ca36ce1
Qiang <qxl0@yahoo.com>
2022-06-09 16:31:29 -0400

    Solved

--- SelfCrossing.py
+++ SelfCrossing.py
@@ -9,7 +9,23 @@ Notice that the solution set must not contain duplicate triplets.
 
 class Solution:
     def isSelfCrossing(self, distance: List[int]) -> bool:
-        pass
+        n = len(distance)
+        x = distance
+        for i, c in enumerate(3, n):
+            if x[i] > x[i - 2] and x[i - 1] < x[i - 3]:
+                return True
+            if i >= 4:
+                if x[i] + x[i - 4] > x[i - 2] and x[i - 1] == x[i - 3]:
+                    return True
+            if i >= 5:
+                if (
+                    x[i] >= x[i - 2] - x[i - 4]
+                    and x[i - 1] >= x[i - 3] - x[i - 5]
+                    and x[i - 2] > x[x - 4]
+                    and x[i - 3] > x[i - 5]
+                ):
+                    return True
+        return False
 
 
 if __name__ == "__main__":
