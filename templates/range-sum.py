6e94ce5e3baff835e5ab17f14e8f3bbaa4ff1dcf
Qiang <qxl0@yahoo.com>
2022-06-12 08:43:48 -0400

    2 range sum

--- templates/range-sum.py
+++ templates/range-sum.py
@@ -1,5 +1,5 @@
 class SegTreeNode:
-    def __init__(self, a, b, val):
+    def __init__(self, a, b, val):  # init [a,b] with val
         self.tag = 0
         self.start = a
         self.end = b
@@ -10,7 +10,7 @@ class SegTreeNode:
         if not self.left:
             self.left = SegTreeNode(a, mid, val)
             self.right = SegTreeNode(mid + 1, b, val)
-            self.info = max(self.left.info, self.right.info)
+            self.info = self.left.info + self.right.info
 
     def pushDown(self):
         if self.tag == 1 and self.left:
@@ -18,35 +18,33 @@ class SegTreeNode:
             self.right.info = self.info
             self.left.tag = 1
             self.right.tag = 1
-            self.tag == 0
+            self.tag = 0
 
-    def updateRange(self, a, b, val):  # [a,b] val
-        if b < self.start or a > self.end:
+    def updateRange(self, a, b, val):  # set range [a,b] with val
+        if b < self.start or self.end < a:
             return
         if a <= self.start and self.end <= b:
-            self.info = val
+            self.info = val * (self.end - self.start + 1)
             self.tag = 1
             return
+
         if self.left:
             self.pushDown()
-            self.left = self.updateRange(a, b, val)
-            self.right = self.updateRange(a, b, val)
-            self.info = max(self.left.info, self.right.info)
+            self.left.updateRange(a, b, val)
+            self.right.updateRange(a, b, val)
+            self.info = self.left.info + self.right.info
 
-    def queryRange(self, a, b):
+    def queryRange(self, a, b):  # query sum over [a,b]
         if b < self.start or a > self.end:
-            return float("-inf")
+            return 0
         if a <= self.start and self.end <= b:
             return self.info
 
         if self.left:
             self.pushDown()
-            ret = max(self.left.queryRange(a, b), self.right.queryRange(a, b))
-            self.info = max(self.left.info, self.right.info)
-            return ret
-
-        # should not reach here
-        return self.info
+            ret = self.left.queryRange(a, b) + self.right.queryRange(a, b)
+            self.info = self.left.info + self.right.info
+            return self.info
 
 
 if __name__ == "__main__":
