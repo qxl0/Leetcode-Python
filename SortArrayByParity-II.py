61dbad4a9efe39715b06028b405fa2d4a4f5c354
Qiang <qxl0@yahoo.com>
2022-06-13 15:14:39 -0400

    Sort Array by Parity II

---
+++ SortArrayByParity-II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sortArrayByParityII(self, A):
+        j = 1
+        for i in range(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
+        return A
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    deck = [4, 2, 5, 7]
+    res = sol.sortArrayByParityII(deck)
+    print(res)
