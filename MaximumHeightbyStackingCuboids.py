f3e42f58fd825222e3975fe86631994cb0eff102
Qiang <qxl0@yahoo.com>
2022-06-12 20:19:02 -0400

    Maximum Height by s tacking Cuboids

---
+++ MaximumHeightbyStackingCuboids.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxHeight(self, cuboids: List[List[int]]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    cuboids = [[50, 45, 20], [95, 37, 53], [45, 23, 12]]
+    res = sol.maxHeight(cuboids)
+    print(res)
