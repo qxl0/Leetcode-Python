1590a8f6fa7643a973f2fa487e28866efed3c816
Qiang <qxl0@yahoo.com>
2022-06-14 11:30:08 -0400

    Solved

--- PartitionArrayIntoThreePartsWithEqualSum.py
+++ PartitionArrayIntoThreePartsWithEqualSum.py
@@ -9,17 +9,29 @@ class Solution:
         if s % 3 != 0:
             return False
         ans = 0
+        cnt = 0
         for i in range(len(arr)):
-            if ans > s // 3:
-                return False
-            elif ans == s // 3:
-                ans = 0
             ans += arr[i]
-        return True
+            if ans == s // 3:
+                cnt += 1
+                ans = 0
+        return ans == 0 and cnt >= 3
+
+
+class Solution2:
+    def canThreePartsEqualSum(self, A: List[int]) -> bool:
+        average, remainder, part, cnt = sum(A) // 3, sum(A) % 3, 0, 0
+        for a in A:
+            part += a
+            if part == average:
+                cnt += 1
+                part = 0
+        return not remainder and cnt >= 3
 
 
 if __name__ == "__main__":
     sol = Solution()
-    arr = [3, 3, 6, 5, -2, 2, 5, 1, -9, 4]
+    # arr = [3, 3, 6, 5, -2, 2, 5, 1, -9, 4]
+    arr = [1, -1, 1, -1]
     res = sol.canThreePartsEqualSum(arr)
     print(res)
