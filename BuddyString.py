33d8b8a6d91a222005eefa33ba4e6be4e53a87dc
Qiang <qxl0@yahoo.com>
2022-06-13 21:05:34 -0400

    Buddy String

---
+++ BuddyString.py
@@ -0,0 +1,29 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+
+        if len(s) != len(goal):
+            return False
+
+        if s == goal:
+            seen = set()
+            for a in s:
+                if a in seen:
+                    return True
+                seen.add(a)
+            return False
+
+        pairs = []
+        for a, b in zip(s, goal):
+            if a != b:
+                pairs.append((a, b))
+            if len(pairs) >= 3:
+                return False
+        return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "ab"
+    goal = "ba"
+    res = sol.buddyStrings(s, goal)
+    print(res)
