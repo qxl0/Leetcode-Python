2d115887532726f99d1673e400fea4ba9e6185c1
Qiang <qxl0@yahoo.com>
2022-06-14 16:22:16 -0400

    Index Pairs of a String

---
+++ IndexPairsofaString.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children, self.is_word = {}, False
+
+    @staticmethod
+    def construct_trie(words):
+        root = TrieNode()
+        for word in words:
+            node = root
+            for c in word:
+                node.children.setdefault(c, TrieNode())
+                node = node.children[c]
+            node.is_word = True
+        return root
+
+
+class Solution:
+    def indexPairs(self, text, words):
+        res, trie = [], TrieNode.construct_trie(words)
+        for l in range(len(text)):
+            node = trie
+            for r in range(l, len(text)):
+                if text[r] not in node.children:
+                    break
+                node = node.children[text[r]]
+                if node.is_word:
+                    res.append((l, r))
+        return res
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    text = "thestoryofleetcodeandme"
+    words = ["story", "fleet", "leetcode"]
+    res = sol.indexPairs(text, words)
+    print(res)
