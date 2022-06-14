03c9769031dcbdbe15f3d25afcaca2cba325ef9b
Qiang <qxl0@yahoo.com>
2022-06-14 11:31:32 -0400

    Remove Outermost Parentheses

---
+++ RemoveOutermostParentheses.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "(()())(())"
+    res = sol.removeOuterParentheses(s)
+    print(res)
