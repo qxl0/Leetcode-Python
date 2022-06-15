d5040e31320898606d4b5d9c0d98861c7b6f0b71
Qiang <qxl0@yahoo.com>
2022-06-15 16:40:56 -0400

    Correct a error

--- DaysofWeek.py
+++ DaysofWeek.py
@@ -15,12 +15,6 @@ class Solution:
         c, y = y / 100, y % 100
         w = (c / 4 - 2 * c + y + y / 4 + 13 * (m + 1) / 5 + d - 1) % 7
         return self.days[w]
-        if m < 3:
-            m += 12
-            y -= 1
-        c, y = y / 100, y % 100
-        w = (c / 4 - 2 * c + y + y / 4 + 13 * (m + 1) / 5 + d - 1) % 7
-        return self.days[w]
 
 
 if __name__ == "__main__":
