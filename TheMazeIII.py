4fd2ebad52e9025fa7e0616cf0d5d787f91c0730
Qiang <qxl0@yahoo.com>
2022-06-16 14:18:26 -0400

    Solved using BFS + PQ

--- TheMazeIII.py
+++ TheMazeIII.py
@@ -29,130 +29,37 @@ DIRECTION_HASH = {
 
 
 class Solution:
-    """
-    @param maze: the maze
-    @param ball: the ball position
-    @param hole: the hole position
-    @return: the lexicographically smallest way
-    """
-
-    def find_shortest_way(
-        self, maze: List[List[int]], ball: List[int], hole: List[int]
+    def findShortestWay(
+        self, maze: List[List[int]], start: List[int], hole: List[int]
     ) -> str:
         m, n = len(maze), len(maze[0])
-        pq = [(0, "", ball[0], ball[1])]
-        dist = [[sys.maxsize / 2] * n for _ in range(m)]
-        directions = {
-            (1, 0): "d",
-            (0, -1): "l",
-            (0, 1): "r",
-            (-1, 0): "u",
-        }  # ==> l, u, r, d
+        hole = tuple(hole)
+        dir = [(1, 0, "d"), (-1, 0, "u"), (0, 1, "r"), (0, -1, "l")]
 
-        def calcNumOfSteps(x, y, dir_x, dir_y, hole, maze):
-            step = 0
-            m, n = len(maze), len(maze[0])
-            while (
-                x + dir_x >= 0
-                and x + dir_x < m
-                and y + dir_y >= 0
-                and y + dir_y < n
-                and maze[x + dir_x][y + dir_y] != 1
-            ):
-                step += 1
-                x, y = x + dir_x, y + dir_y
-                if x == hole[0] and y == hole[1]:
-                    break
-            return step
-
-        ret = ""
+        pq = [(0, start[0], start[1], "")]
+        vis = [[0] * n for _ in range(m)]
         while pq:
-            d, s, x, y = heapq.heappop(pq)
-            if d > dist[x][y]:
+            d, i, j, path = heapq.heappop(pq)
+            print(d, " : ", i, j, path)
+            if (i, j) == hole:
+                return path
+            if vis[i][j] == 1:
                 continue
-            else:
-                dist[x][y] = d
-            if x == hole[0] and y == hole[1]:
-                ret = s
-                break
-            for dir_x, dir_y in directions:
-                step = calcNumOfSteps(x, y, dir_x, dir_y, hole, maze)
-                new_x, new_y = x + dir_x * step, y + dir_y * step
-                if d + step >= dist[new_x][new_y]:
+            vis[i][j] = 1
+
+            # d, 4 dirs
+            for dx, dy, op in dir:
+                x, y, step = i, j, 0
+                while m > x + dx >= 0 and n > y + dy >= 0 and maze[x + dx][y + dy] == 0:
+                    step += 1
+                    x += dx
+                    y += dy
+                    if (x, y) == hole:
+                        break
+                if vis[x][y] == 1:
                     continue
-                heapq.heappush(
-                    pq, (d + step, s + directions[(dir_x, dir_y)], new_x, new_y)
-                )
-        return ret if ret != "" else "impossible"
-
-
-class MazeGridType:
-    SPACE = 0
-    WALL = 1
-
-
-class Solution2:
-    """
-    @param maze: the maze
-    @param ball: the ball position
-    @param hole: the hole position
-    @return: the lexicographically smallest way
-    """
-
-    def findShortestWay(self, maze, ball, hole):
-        # corner case check
-        if not ball or not hole:
-            return "impossible"
-        if not maze or not maze[0]:
-            return "impossible"
-
-        hole = (hole[0], hole[1])
-
-        # (distance, x, y, path)
-        queue = collections.deque([(ball[0], ball[1])])
-        distance = {(ball[0], ball[1]): (0, "")}
-
-        while queue:
-            x, y = queue.popleft()
-            dist, path = distance[(x, y)]
-
-            for direction in DIRECTION_HASH:
-                if path and path[-1] == direction:
-                    continue
-
-                new_x, new_y = self.kick_ball(x, y, direction, maze, hole)
-                new_dist = dist + abs(new_x - x) + abs(new_y - y)
-                new_path = path + direction
-                if (new_x, new_y) in distance and distance[(new_x, new_y)] <= (
-                    new_dist,
-                    new_path,
-                ):
-                    continue
-
-                queue.append((new_x, new_y))
-                distance[(new_x, new_y)] = (new_dist, new_path)
-
-        if hole in distance:
-            return distance[hole][1]
-
+                heapq.heappush(pq, (d + step, x, y, path + op))
         return "impossible"
-
-    def kick_ball(self, x, y, direction, maze, hole):
-        # kick ball through direction from x, y and return the stopped position
-        dx, dy = DIRECTION_HASH[direction]
-        while (x, y) != hole and not self.is_wall(x, y, maze):
-            x += dx
-            y += dy
-
-        if (x, y) == hole:
-            return x, y
-
-        return x - dx, y - dy
-
-    def is_wall(self, x, y, maze):
-        if not (0 <= x < len(maze) and 0 <= y < len(maze[0])):
-            return True
-        return maze[x][y] == MazeGridType.WALL
 
 
 if __name__ == "__main__":
