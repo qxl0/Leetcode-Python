9ae66c6fd730c5e43afb9fc0dc1b91d7ee0806c5
Qiang <qxl0@yahoo.com>
2022-06-18 10:55:35 -0400

    Another solution

--- CountVowelSubstringsofaString.py
+++ CountVowelSubstringsofaString.py
@@ -33,8 +33,28 @@ class Solution:
         return ret
 
 
+class Solution2:
+    def countVowelSubstrings(self, word: str) -> int:
+        vowels = ("a", "e", "i", "o", "u")
+
+        result = 0
+        start = 0
+        vowel_idx = {}
+        for idx, c in enumerate(word):
+            if c in vowels:
+                if not vowel_idx:
+                    start = idx
+                vowel_idx[c] = idx
+                if len(vowel_idx) == 5:
+                    result += min(vowel_idx.values()) - start + 1
+            elif vowel_idx:
+                vowel_idx = {}
+
+        return result
+
+
 if __name__ == "__main__":
-    sol = Solution()
+    sol = Solution2()
     word = "aeuioauo"
     res = sol.countVowelSubstrings(word)
     print("result is: ", res)
