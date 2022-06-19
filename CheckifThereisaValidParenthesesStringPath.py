71a1d97a19fb02c04e2b290e84a82ac4e5fe2437
Qiang <qxl0@yahoo.com>
2022-06-19 10:23:37 -0400

    Another solution

--- CheckifThereisaValidParenthesesStringPath.py
+++ CheckifThereisaValidParenthesesStringPath.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Counter, List
 
 
@@ -14,18 +15,28 @@ class Solution:
                 if j >= 1:
                     # print(dp[i][j-1])
                     for x in dp[i][j - 1]:
-                        if x + k >= 0:  # and (m+n+1-(i+j+1))<=x+k:
+                        if x + k >= 0 and (m + n + 1 - (i + j + 1)) >= x + k:
                             dp[i][j].add(x + k)
                 if i >= 1:
                     for x in dp[i - 1][j]:
-                        if x + k >= 0:  # and (m+n+1-(i+j+1))<=x+k:
+                        if x + k >= 0 and (m + n + 1 - (i + j + 1)) >= x + k:
                             dp[i][j].add(x + k)
         print(dp)
         return 0 in dp[m - 1][n - 1]
+
+    def hasValidPath2(self, A):
+        m, n = len(A), len(A[0])
+        dp = defaultdict(set)
+        dp[0, -1] = dp[-1, 0] = {0}
+        for i in range(m):
+            for j in range(n):
+                d = 1 if A[i][j] == "(" else -1
+                dp[i, j] |= {a + d for a in dp[i - 1, j] | dp[i, j - 1] if a + d >= 0}
+        return 0 in dp[m - 1, n - 1]
 
 
 if __name__ == "__main__":
     sol = Solution()
     grid = [["(", "(", "("], [")", "(", ")"], ["(", "(", ")"], ["(", "(", ")"]]
-    res = sol.hasValidPath(grid)
+    res = sol.hasValidPath2(grid)
     print("result is: ", res)
