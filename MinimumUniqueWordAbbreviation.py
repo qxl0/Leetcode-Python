3a193d07d6e286807510aa34adb8467a6a15cf59
Qiang <qxl0@yahoo.com>
2022-07-04 21:18:51 -0400

    bitmask solution

--- MinimumUniqueWordAbbreviation.py
+++ MinimumUniqueWordAbbreviation.py
@@ -1,19 +1,64 @@
 class Solution:
+    # def minAbbreviation(self, target, dictionary):
+    #     m = len(target)
+    #     diffs = {
+    #         sum(2**i for i, c in enumerate(word) if target[i] != c)
+    #         for word in dictionary
+    #         if len(word) == m
+    #     }
+    #     if not diffs:
+    #         return str(m)
+    #     bits = max(
+    #         (i for i in range(2**m) if all(d & i for d in diffs)),
+    #         key=lambda bits: sum((bits >> i) & 3 == 0 for i in range(m - 1)),
+    #     )
+    #     s = "".join(target[i] if bits & 2**i else "#" for i in range(m))
+    #     return re.sub("#+", lambda m: str(len(m.group())), s)
     def minAbbreviation(self, target, dictionary):
+        def abbr(target, num):
+            old = num
+            word, count = "", 0
+            for w in target:
+                if num & 1 == 1:
+                    if count:
+                        word += str(count)
+                        count = 0
+                    word += w
+                else:
+                    count += 1
+
+                num >>= 1
+            if count:
+                word += str(count)
+            print(bin(old), word)
+            return word
+
         m = len(target)
-        diffs = {
-            sum(2**i for i, c in enumerate(word) if target[i] != c)
-            for word in dictionary
-            if len(word) == m
-        }
+
+        # Figure out the different bits for a same length word in the dictionary
+        diffs = []
+        for word in dictionary:
+            if len(word) != m:
+                continue
+
+            # The encoding is opposite
+            bits = 0
+            for i, char in enumerate(word):
+                if char != target[i]:
+                    bits += 2**i
+            diffs += (bits,)
+
+        # No word in dictionary has same length, return the shortest
         if not diffs:
             return str(m)
-        bits = max(
-            (i for i in range(2**m) if all(d & i for d in diffs)),
-            key=lambda bits: sum((bits >> i) & 3 == 0 for i in range(m - 1)),
-        )
-        s = "".join(target[i] if bits & 2**i else "#" for i in range(m))
-        return re.sub("#+", lambda m: str(len(m.group())), s)
+
+        abbrs = []
+        for i in range(2**m):
+            # This abbreviation at least has one word different to every words in the dictionary
+            if all(d & i for d in diffs):
+                abbrs += (abbr(target, i),)
+
+        return min(abbrs, key=lambda x: len(x))
 
 
 if __name__ == "__main__":
