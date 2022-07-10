96bb51e060e62eec9a41a427ed8e9734e5a23de7
Qiang <qxl0@yahoo.com>
2022-07-10 18:48:40 -0400

    Fixed minisweeper game

--- MineSweeperGame.py
+++ MineSweeperGame.py
@@ -10,52 +10,52 @@ from sortedcontainers import SortedDict
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         m, n = len(board), len(board[0])
-        dt = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-        dt2 = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
-
-        def mines(x, y):
-            # check 8 dirs num of mines
-            ret = 0
-            for dx, dy in dt + dt2:
-                nx, ny = x + dx, y + dy
-                if nx < 0 or nx >= m or ny < 0 or ny >= n:
-                    continue
-                if board[nx][ny] == "M":
-                    ret += 1
-            return ret
+        dt = [(-1, 0), (1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
         q = [(click)]
         vis = set()
         vis.add((click[0], click[1]))
         while q:
-            print(q)
+            # print(q)
             x, y = q.pop(0)
-            if board[x][y] == "E":
-                num_m = mines(x, y)
-                board[x][y] = "B" if num_m == 0 else str(num_m)
-            elif board[x][y] == "M":
-                board[x][y] = "X"
-
+            count = 0
+            nxt = []
             for dx, dy in dt:
                 nx, ny = x + dx, y + dy
                 if nx < 0 or nx >= m or ny < 0 or ny >= n:
                     continue
-                if (nx, ny) in vis:
-                    continue
-                vis.add((nx, ny))
-                if board[nx][ny] in ["E", "M"]:
-                    q.append((nx, ny))
+                if board[nx][ny] == "M":
+                    count += 1
+                else:
+                    nxt.append((nx, ny))
+
+            if count == 0:
+                board[x][y] = "B"
+                for nx, ny in nxt:
+                    if (nx, ny) not in vis:
+                        vis.add((nx, ny))
+                        q.append((nx, ny))
+            else:
+                board[x][y] = str(count)
+
         return board
 
 
 if __name__ == "__main__":
     sol = Solution()
+    # board = [
+    #     ["E", "E", "E", "E", "E"],
+    #     ["E", "E", "M", "E", "E"],
+    #     ["E", "E", "E", "E", "E"],
+    #     ["E", "E", "E", "E", "E"],
+    # ]
+    # click = [3, 0]
     board = [
-        ["E", "E", "E", "E", "E"],
-        ["E", "E", "M", "E", "E"],
-        ["E", "E", "E", "E", "E"],
-        ["E", "E", "E", "E", "E"],
+        ["B", "1", "E", "1", "B"],
+        ["B", "1", "M", "1", "B"],
+        ["B", "1", "1", "1", "B"],
+        ["B", "B", "B", "B", "B"],
     ]
-    click = [3, 0]
+    click = [1, 2]
     res = sol.updateBoard(board, click)
     print(res)
