c6c67f767671cbe09d0443af282c3782df714e08
Qiang <qxl0@yahoo.com>
2022-07-21 16:32:20 -0400

    Score of Parentheses

---
+++ ScoreofParentheses.py
@@ -0,0 +1,32 @@
+from collections import Counter
+import heapq
+from math import inf
+from typing import List
+
+
+class Solution(object):
+    def scoreOfParentheses(self, S):
+        def F(i, j):
+            # Score of balanced string S[i:j]
+            ans = bal = 0
+
+            # Split string into primitives
+            for k in xrange(i, j):
+                bal += 1 if S[k] == "(" else -1
+                if bal == 0:
+                    if k - i == 1:
+                        ans += 1
+                    else:
+                        ans += 2 * F(i + 1, k)
+                    i = k + 1
+
+            return ans
+
+        return F(0, len(S))
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    S = "()()"
+    res = sol.scoreOfParentheses(S)
+    print(res)
