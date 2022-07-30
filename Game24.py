a13b3d5e04dabdecdda23787b0940c622df7029d
Qiang <qxl0@yahoo.com>
2022-07-30 17:04:20 -0400

    Added expr as result

--- Game24.py
+++ Game24.py
@@ -5,31 +5,33 @@ from itertools import permutations
 
 
 class Solution:
-    def judgePoint24(self, cards: List[int]) -> bool:
-        def helper(nums, l, r):
-            if l == r:
-                return set([nums[l]])
-            rets = set()
-            for i in range(l, r):
-                A = helper(nums, l, i)
-                B = helper(nums, i + 1, r)
-                for x in A:
-                    for y in B:
-                        rets.add(x + y)
-                        rets.add(x - y)
-                        rets.add(x * y)
-                        if y != 0:
-                            rets.add(x / y)
-            return rets
+    class Solution:
+        def judgePoint24(self, cards: List[int]) -> bool:
+            def helper(nums, l, r):
+                if l == r:
+                    return set([(nums[l], f"{nums[l]}")])
+                rets = set()
+                for i in range(l, r):
+                    A = helper(nums, l, i)
+                    B = helper(nums, i + 1, r)
+                    for x, x1 in A:
+                        for y, y1 in B:
+                            rets.add((x + y, f"({x1})+({y1})"))
+                            rets.add((x - y, f"({x1})-({y1})"))
+                            rets.add((x * y, f"({x1})*({y1})"))
+                            if y != 0:
+                                rets.add((x / y, f"({x})/({y})"))
+                return rets
 
-        p = permutations(cards)
-        for nums in list(p):
-            # compute 24 for nums
-            rets = helper(nums, 0, 3)
-            for x in rets:
-                if abs(x - 24) < 1e-10:
-                    return True
-        return False
+            p = permutations(cards)
+            for nums in list(p):
+                # compute 24 for nums
+                rets = helper(nums, 0, 3)
+                for x, y in rets:
+                    if abs(x - 24) < 1e-10:
+                        print(y)
+                        return True
+            return False
 
 
 if __name__ == "__main__":
