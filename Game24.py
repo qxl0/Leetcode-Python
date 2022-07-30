a3b9650bfe48b6f8df5be27e992189a2628afaed
Qiang <qxl0@yahoo.com>
2022-07-30 16:43:44 -0400

    24 Game

---
+++ Game24.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    cards = [4, 1, 8, 9]
+    res = sol.judgePoint24(cards)
+    print(res)
