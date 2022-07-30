a7542387e34f56b453627eb9605295db546d0918
Qiang <qxl0@yahoo.com>
2022-07-30 13:32:16 -0400

    My Calendar

--- MyCalendar-I.py
+++ MyCalendar-I.py
@@ -1,9 +1,35 @@
+class Node:
+    __slots__ = "start", "end", "left", "right"
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = self.right = None
+
+    def insert(self, node):
+        if self.end <= node.start:
+            if not self.left:
+                self.left = node
+                return True
+            return self.left.insert(node)
+        elif node.end <= self.start:
+            if not self.right:
+                self.right = node
+                return True
+            return self.right.insert(node)
+        else:
+            return False
+
+
 class MyCalendar:
     def __init__(self):
-        pass
+        self.root = None
 
     def book(self, start, end):
-        pass
+        if not self.root:
+            self.root = Node(start, end)
+            return True
+        return self.root.insert(Node(start, end))
 
 
 if __name__ == "__main__":
