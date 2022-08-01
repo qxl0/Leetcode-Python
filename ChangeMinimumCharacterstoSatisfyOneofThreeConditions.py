44f5a68aa8a9b27126888e0da38dda89152b898e
Qiang <qxl0@yahoo.com>
2022-08-01 15:42:33 -0400

    Fixed bug

--- ChangeMinimumCharacterstoSatisfyOneofThreeConditions.py
+++ ChangeMinimumCharacterstoSatisfyOneofThreeConditions.py
@@ -9,40 +9,38 @@ class Solution:
     def minCharacters(self, a: str, b: str) -> int:
         ans = inf
         aCount, bCount = Counter(a), Counter(b)
-        for i in "abcdefghijklmnopqrstuvwxyz":
-            # check a
-            op1 = 0
-            for ch, cnt in aCount.items():
-                if ch != i:
-                    op1 += cnt
-            for ch, cnt in bCount.items():
-                if ch != i:
-                    op1 += cnt
-            ans = min(ans, op1)
+        for th in range(1, 26):
+            # print(chr(ord('a')+th))
+            change = 0
+            for i in range(th, 26):
+                # print(chr(ord('a')+i))
+                change += aCount[chr(ord("a") + i)]
+            for i in range(th):
+                change += bCount[chr(ord("a") + i)]
+            # print(change)
+            ans = min(ans, change)
 
-            op3 = 0  # all ch in a < b
-            for ch, cnt in aCount.items():
-                if ch >= i:
-                    op3 += cnt
-            for ch, cnt in bCount.items():
-                if ch < i:
-                    op3 += cnt
-            ans = min(ans, op3)
+            change = 0
+            for i in range(th, 26):
+                change += bCount[chr(ord("a") + i)]
+            for i in range(th):
+                change += aCount[chr(ord("a") + i)]
+            # print(change)
+            ans = min(ans, change)
 
-            op2 = 0  # all ch in b < a
-            for ch, cnt in bCount.items():
-                if ch >= i:
-                    op2 += cnt
-            for ch, cnt in aCount.items():
-                if ch < i:
-                    op2 += cnt
-            ans = min(ans, op2)
+        for th in range(26):
+            change = 0
+            for i in range(26):
+                if i != th:
+                    change += aCount[chr(ord("a") + i)]
+                    change += bCount[chr(ord("a") + i)]
+            ans = min(ans, change)
 
         return ans
 
 
 if __name__ == "__main__":
-    sol = Solution()
+    sol = Solution("a" "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
     a = "a"
     b = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
     res = sol.minCharacters(a, b)
